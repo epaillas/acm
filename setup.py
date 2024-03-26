43e117759599b68ecbab16c973b2ce826d8e90fd
epaillas <enrique.paillas@waterloo.ca>
2024-03-26 07:19:48 -0700

    update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,13 @@
-from setuptools import setup
+import os
+import sys
+from setuptools import setup, find_packages
 
-setup()
+
+package_basename = 'acm'
+package_dir = os.path.join(os.path.dirname(__file__), package_basename)
+sys.path.insert(0, package_dir)
+
+setup(
+    name=package_basename,
+    packages=find_packages(),
+)
