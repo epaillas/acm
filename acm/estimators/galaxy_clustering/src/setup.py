7e8200f23a7ca1c806398d1c7542fcadecde64b2
Daniel Forero <daniel.forerosanchez@epfl.ch>
2024-05-16 08:26:54 -0700

    First implementation of DT spheres/voids into ACM pipeline

--- acm/estimators/galaxy_clustering/src/setup.py
+++ acm/estimators/galaxy_clustering/src/setup.py
@@ -14,9 +14,30 @@ ext_modules = [
     libraries=["m"],
     extra_compile_args = ['-O3','-ffast-math'],
     include_dirs=[numpy.get_include()]),
+    Extension("pydive",
+        sources=['acm/estimators/galaxy_clustering/src/pydive.pyx',
+                  ],
+        include_dirs=[numpy.get_include(), 
+                      '/global/u1/d/dforero/lib/CGAL-5.4/include', 
+                      ],
+                      
+        library_dirs=[
+                      ],
+        libraries=['m', 'gsl', 'gslcblas', 'gmp', 'mpfr'],
+        language='c++',
+        extra_compile_args=['-fPIC', '-fopenmp'],
+        extra_link_args=['-fopenmp']
+    )
 ]
+
+
+
 
 setup(
   name = "acm",
   cmdclass = {"build_ext": build_ext},
-  ext_modules = ext_modules)
+  ext_modules = ext_modules,
+  install_requires=[
+        'numpy',
+        'kymatio',
+    ],)
