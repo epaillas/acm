1adb4857c26ad2b6308dff9cada4ef748ac3faaa
florpi <cuestalz@mit.edu>
2024-09-05 05:12:50 -0700

    updated knn script

--- scripts/emc/run_knn.py
+++ scripts/emc/run_knn.py
@@ -7,10 +7,10 @@ import numpy as np
 
 setup_logging()
 
-def get_data(cosmology, phase, hod, boxsize):
+def get_data(data_dir, cosmology, phase, hod, boxsize):
     # read some random galaxy catalog
-    data_dir = f'/pscratch/sd/e/epaillas/emc/hods/z0.5/yuan23_prior2/c{str(cosmology).zfill(3)}_ph{str(phase).zfill(3)}/'
-    data_fn = Path(data_dir) / f'hod{str(hod).zfill(3)}.npy'
+    data_dir = data_dir / f'hods/cosmo+hod/z0.5/yuan23_prior/c{str(cosmology).zfill(3)}_ph{str(phase).zfill(3)}/'
+    data_fn = data_dir / f'hod{str(hod).zfill(3)}.npy'
     data = np.load(data_fn, allow_pickle=True).item()
     x = data['x'] % boxsize
     y = data['y'] % boxsize
@@ -32,21 +32,30 @@ if __name__ == '__main__':
     n_hods = 1000
     boxsize = 2000.
     ks = [1, 2,3,4,5,6,7,8,9]
-    rps = np.logspace(-0.2, 1.8, 20)
-    pis = np.logspace(-0.3, 1.5, 15)
+    rp_start, rp_end = 0.63, 63
+    pi_start, pi_end = 0.5, 32
+    rps = np.logspace(np.log10(rp_start), np.log10(rp_end), 8)
+    pis = np.logspace(np.log10(pi_start), np.log10(pi_end), 5)
     knn = KthNearestNeighbor(boxsize=boxsize, boxcenter=boxsize/2, cellsize=5.0)
+    data_dir = Path(f'/pscratch/sd/e/epaillas/emc/')
 
-    lhc_y = []
-    for hod in range(n_hods):
-        print(f'Doing hod = {hod}')
-        data_positions = get_data(0, 0, hod, boxsize)
-        pos_rand = get_randoms(n_randoms=10000, boxsize=boxsize)
-        lhc_y.append(knn.run_knn(rps, pis, data_positions, pos_rand, kneighbors = ks))
+
+    cosmologies = list(range(4)) + list(range(100, 182))
+    lhc_x, lhc_y = [], []
+    for cosmolgoy in cosmologies:
+        params = pd.read_csv(data_dir / f'cosmo+hod_params/AbacusSummit_c{str(cosmology).zfill(3)}.csv')
+        lhc_x.append(params.values[:n_hods])
+        for hod in range(n_hods):
+            print(f'Doing hod = {hod}')
+            data_positions = get_data(data_dir, cosmology, 0, hod, boxsize)
+            pos_rand = get_randoms(n_randoms=10000, boxsize=boxsize)
+            lhc_y.append(knn.run_knn(rps, pis, data_positions, pos_rand, kneighbors = ks))
+
+    # read cosmo + hod paramters
     lhc_y = np.array(lhc_y)
-    lhc_x = pd.read_csv('/pscratch/sd/e/epaillas/emc/hod_params/yuan23/hod_params_yuan23_c000.csv')
-    lhc_x_names = list(lhc_x.columns)
-    lhc_x_names = [name.replace(' ', '').replace('#', '') for name in lhc_x_names]
-    lhc_x = lhc_x.values[:len(lhc_y),:]
+    print('lhc_y = ', lhc_y.shape)
+    lhc_x = np.concatenate(lhc_x)
+    print('lhc_x = ', lhc_x.shape)
 
     rps_c = 0.5*(rps[1:]+rps[:-1])
     pis_c = 0.5*(pis[1:]+pis[:-1])
