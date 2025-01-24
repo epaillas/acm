837bea3bdb25c5fa885675f178d570140f8ae775
SBouchard01 <simonbouchard47@gmail.com>
2025-01-24 13:26:42 -0800

    Set filters to class properties, added prediction filtering too

--- acm/observables/base.py
+++ acm/observables/base.py
@@ -5,7 +5,7 @@ import numpy as np
 import torch
 
 from sunbird.emulators import FCN
-from acm.data.io_tools import read_lhc, read_covariance_y, read_covariance, read_model
+from acm.data.io_tools import read_lhc, read_covariance_y, summary_coords, read_model, filter
 
 # TODO : improve the docstrings
 
@@ -13,13 +13,23 @@ class BaseObservable(ABC):
     """
     Base class for the statistics results handling in the ACM pipeline.
     """
-    def __init__(self):
+    def __init__(self, select_filters: dict = None, slice_filters: dict = None):
         """
-        Expecting a self.stat_name to be defined here ! 
+        Expecting setting up : 
+        - self.stat_name : name of the statistic (class attribute)
         """
+        self.select_filters = select_filters
+        self.slice_filters = slice_filters
     
+    #%% Properties : Define the class properties (name, paths, statistics coordinates, etc.)
+    @property
+    @abstractmethod
+    def stat_name(self) -> str:
+        """
+        Name of the statistic.
+        """
+        pass
     
-    #%% Properties : Define the class properties (paths, statistics coordinates, etc.)
     @property  
     @abstractmethod
     def paths(self) -> dict:
@@ -67,28 +77,40 @@ class BaseObservable(ABC):
             summary_coords_dict = self.summary_coords_dict
             )
     
-    def lhc_x(self, select_filters=None, slice_filters=None):
+    def lhc_x(self):
         """
         Latin hypercube of input features (cosmological and/or HOD parameters)
         """
-        lhc_x, lhc_y, lhc_x_names = self.read_lhc(select_filters=select_filters, slice_filters=slice_filters, return_sep=False)
+        lhc_x, lhc_y, lhc_x_names = self.read_lhc(
+            select_filters=self.select_filters, 
+            slice_filters=self.slice_filters, 
+            return_sep=False
+            )
         return lhc_x
     
-    def lhc_y(self, select_filters=None, slice_filters=None):
+    def lhc_y(self):
         """
         Latin hypercube of output features (tpcf, power spectrum, etc).
         """
-        lhc_x, lhc_y, lhc_x_names = self.read_lhc(select_filters=select_filters, slice_filters=slice_filters, return_sep=False)
+        lhc_x, lhc_y, lhc_x_names = self.read_lhc(
+            select_filters=self.select_filters, 
+            slice_filters=self.slice_filters, 
+            return_sep=False
+            )
         return lhc_y
     
-    def bin_values(self, select_filters=None, slice_filters=None):
+    def bin_values(self):
         """
         Bin values for the statistic. (e.g. separation bins for the correlation function)
         """
-        bin_values, lhc_x, lhc_y, lhc_x_names = self.read_lhc(select_filters=select_filters, slice_filters=slice_filters, return_sep=True)
+        bin_values, lhc_x, lhc_y, lhc_x_names = self.read_lhc(
+            select_filters=self.select_filters, 
+            slice_filters=self.slice_filters, 
+            return_sep=True
+            )
         return bin_values
     
-    def covariance_y(self, select_filters=None, slice_filters=None):
+    def covariance_y(self):
         """
         Output features from the small AbacusSummit box for covariance
         estimation.
@@ -98,16 +120,20 @@ class BaseObservable(ABC):
         return read_covariance_y(
             statistic=self.stat_name,
             data_dir=data_dir,
-            select_filters=select_filters,
-            slice_filters=slice_filters,
+            select_filters=self.select_filters,
+            slice_filters=self.slice_filters,
             summary_coords_dict=self.summary_coords_dict,
             )
     
-    def covariance_matrix(self, select_filters=None, slice_filters=None,  volume_factor=64, prefactor=1):
+    def covariance_matrix(
+        self,
+        volume_factor: float = 64, 
+        prefactor: float = 1):
         """
         Covariance matrix for the statistic. 
-        """
-        cov_y = self.covariance_y(select_filters=select_filters, slice_filters=slice_filters)
+        The prefactor is here for corrections if needed, and the volume factor is the volume correction of the boxes.
+        """   
+        cov_y = self.covariance_y(select_filters=self.select_filters, slice_filters=self.slice_filters)
         prefactor = prefactor / volume_factor
         
         cov = prefactor * np.cov(cov_y, rowvar=False) # rowvar=False : each column is a variable and each row is an observation
@@ -132,7 +158,7 @@ class BaseObservable(ABC):
             model_fn = self.paths['model_dir'] + f'{self.stat_name}/' + self.paths['checkpoint_name']
         return read_model([self.stat_name], model_fn)[0]
     
-    def get_prediction(self, model, x, to_numpy=True):
+    def get_model_prediction(self, model, x)-> np.ndarray:
         """
         Get the prediction from the model.
         
@@ -142,8 +168,6 @@ class BaseObservable(ABC):
             Trained theory model.
         x : array_like
             Input features.
-        to_numpy : bool, optional
-            If True, the prediction is converted to a numpy array. Defaults to True.
         
         Returns
         -------
@@ -152,8 +176,19 @@ class BaseObservable(ABC):
         """
         with torch.no_grad():
             pred = model.get_prediction(torch.Tensor(x))
-        if to_numpy:
-            return pred.numpy()
+            pred.numpy()
+    
+        # Expect output to be in unfiltered format, i.e. with the same dimensions as summary_coords_dict
+        bin_values, lhc_x, lhc_y, lhc_x_names = self.read_lhc(return_sep=True) # Get unfiltered bin values
+        coords = summary_coords(
+            self.stat_name, 
+            coord_type='emulator_error', # To get only the statistics coordinates
+            bin_values=bin_values,
+            summary_coords_dict=self.summary_coords_dict
+            )
+        coords = {'n_pred': list(range(len(pred))), **coords} # Add extra coordinate for the number of predictions
+        pred = filter(pred, coords, self.select_filters, self.slice_filters, n_sim=len(pred))
+        
         return pred
     
     #%% LHC creation : Methods to create the LHC data from statistics files
