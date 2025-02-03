afdbdf7f8b8fffc2ea7b8cae8fc16b2b87492bf8
SBouchard01 <simonbouchard47@gmail.com>
2025-02-03 07:08:17 -0800

    feat: add CombinedModel class for combining theory models

--- acm/observables/combined.py
+++ acm/observables/combined.py
@@ -1,6 +1,35 @@
 import numpy as np
 from .base import BaseObservable
 
+class CombinedModel():
+    """
+    Class for the combination of theory models.
+    """
+    def __init__(self, observables: list[BaseObservable]):
+        """
+        Parameters
+        ----------
+        observables : list[BaseObservable]
+            List of observables to be combined, initialized with their respective filters.
+        """
+        self.observables = observables
+        self.models = [obs.model for obs in self.observables]
+
+    def get_prediction(self, x):
+        """
+        Get the prediction from the model.
+        
+        Parameters
+        ----------
+        x : array_like
+            Input features.
+        
+        Returns
+        -------
+        array_like
+            Model prediction, with respective filters applied to each observable.
+        """
+        return np.concatenate([obs.get_model_prediction(x) for obs in self.observables], axis=-1)
 
 class BaseCombinedObservable(BaseObservable):
     """
@@ -24,14 +53,14 @@ class BaseCombinedObservable(BaseObservable):
         """
         Defines the default paths for the statistics results.
         """
-        return [obs.paths for obs in self.observables]
+        return {obs.stat_name: obs.paths for obs in self.observables} 
     
     @property
     def summary_coords_dict(self):
         """
         Defines the default coordinates for the statistics results.
         """
-        return [obs.summary_coords_dict for obs in self.observables]
+        return {obs.stat_name: obs.summary_coords_dict for obs in self.observables} 
 
     @property
     def lhc_x(self):
@@ -75,11 +104,20 @@ class BaseCombinedObservable(BaseObservable):
         return np.concatenate([obs.covariance_y for obs in self.observables], axis=-1)
 
     @property
+    def models(self):
+        """
+        Dict of theory models of the combination of observables.
+        """
+        return {obs.stat_name: obs.model for obs in self.observables}
+    
+    @property
     def model(self):
         """
-        Theory model of the combination of observables.
+        Theory model of the combination of observables. 
+        `model.get_prediction(x)` returns the prediction of the combination of observables, 
+        with the respective filters applied to each observable.
         """
-        return [obs.model for obs in self.observables]
+        return CombinedModel(self.observables)
     
     def get_model_prediction(self, x)-> np.ndarray:
         """
@@ -89,9 +127,7 @@ class BaseCombinedObservable(BaseObservable):
         ----------
         x : array_like
             Input features.
-        model : FCN
-            Trained theory model. If None, the model attribute of the class is used. Defaults to None.
-        
+            
         Returns
         -------
         array_like
