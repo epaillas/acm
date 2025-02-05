50d76d8a3c6205acb8fdb3e0dae9fb9088fa4939
epaillas <enrique.paillas@uwaterloo.ca>
2025-02-05 10:43:13 -0800

    update classes

--- acm/observables/emc/combined.py
+++ acm/observables/emc/combined.py
@@ -46,6 +46,13 @@ class CombinedObservable(BaseObservable):
         Features from small AbacusSummit box for covariance estimation.
         """
         return np.concatenate([obs.small_box_y for obs in self.observables], axis=1)
+    
+    @property
+    def diffsky_y(self):
+        """
+        Measurements from Diffsky simulations.
+        """
+        return np.concatenate([obs.diffsky_y for obs in self.observables])
 
     @property
     def model(self):
