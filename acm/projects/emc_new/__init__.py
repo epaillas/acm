d26a1c5d81833bcc587e144fd7df4a32802e0ce3
SBouchard01 <simonbouchard47@gmail.com>
2025-02-06 07:33:21 -0800

    added default import to init files

--- acm/projects/emc_new/__init__.py
+++ acm/projects/emc_new/__init__.py
@@ -1,3 +1,5 @@
+from .default import emc_paths, emc_summary_coords_dict
+
 from .tpcf import GalaxyCorrelationFunctionMultipoles
 from .density_split_correlation import DensitySplitCorrelationFunctionMultipoles
 from .power_spectrum import GalaxyPowerSpectrumMultipoles
