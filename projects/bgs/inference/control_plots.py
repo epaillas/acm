9da0a12367d4a9c59a6119359243abc2507a089b
SBouchard01 <simonbouchard47@gmail.com>
2025-02-18 05:43:04 -0800

    added triangle plot with several chains to the inference control plots

--- projects/bgs/inference/control_plots.py
+++ projects/bgs/inference/control_plots.py
@@ -1,31 +1,78 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from getdist import plots, MCSamples
+from sunbird.inference import BaseSampler
 
-def plot_triangle(chain, priors, labels, ranges, fixed_parameters, save_fn=None, add_bestfit=False, **kwargs):
-        """Plot triangle plot
-        """
-        import matplotlib.pyplot as plt
-        names = [param for param in priors.keys() if param not in fixed_parameters]
-        labels = [labels[param].strip('$') for param in names]
-        data = chain
-        samples = MCSamples(samples=data['samples'], weights=data['weights'], names=names,
-                            loglikes=data['log_likelihood'], labels=labels, ranges=ranges)
+class ControlPlots(BaseSampler):
+    def __init__(self):
+        self.chains = []
+        self.samplers = []
+    
+    def load_chain(self, chain_fn: str, label: str = None):
+        # Load chain
+        chain = np.load(chain_fn, allow_pickle=True).item()
+        chain['label'] = label
+        self.chains.append(chain)
+        
+        # For later
+        # sampler = BaseSampler().load_chain(chain_fn)
+        # chain = sampler.get_chain()
+        # self.chains.append(chain)
+        # self.samplers.append(sampler)
+        
+        # Setup 
+        setup_keys = ['ranges', 'names', 'labels',  'markers']
+        for key in setup_keys:
+            if getattr(self, key, None) is None:
+                setattr(self, key, chain[key])
+            else: # Check that the key is the same for all chains
+                assert getattr(self, key) == chain[key], f'{key} does not match'
+            
+        return chain
+    
+    def plot_triangle(self, add_bestfit=False, **kwargs):
+        
+        # Note : Ignore the thin argument, as it causes warnings in the getdist package
+        colors = kwargs.get('colors', ['k'] + [f'C{i}' for i in range(len(self.chains))])
+        label_dict = kwargs.get('label_dict', {}) 
+        
+        samples = []
+        for chain in self.chains:
+            names = chain['names']
+            labels = [chain['labels'][param].strip('$') for param in names]
+            label = label_dict.get(chain['label'], chain['label']) # replace label with actual name if provided
+
+            sample = MCSamples(
+                samples = chain['samples'],
+                ranges = chain['ranges'],
+                label = label, # Name of the chain (getdist)
+                labels = labels, # Names of the parameters
+                names = names,
+                weights = chain.get('weights', None),
+                loglikes = chain.get('log_likelihood', None),
+            )
+            samples.append(sample)
+        
         g = plots.get_subplot_plotter()
-        g.triangle_plot(samples, **kwargs)
-        maxl = data['samples'][data['log_likelihood'].argmax()]
+        g.triangle_plot(
+            samples, 
+            line_args=[{'color': colors[i]} for i in range(len(samples))],
+            contour_colors=colors, 
+            **kwargs)
+        
         if add_bestfit:
-            params = kwargs['params'] if 'params' in kwargs else names
-            ndim = len(params)
-            finished = []
-            ax_idx = 0
-            for i, param1 in enumerate(params):
-                for j, param2 in enumerate(params[::-1]):
-                    if param2 in finished: continue
-                    if param1 != param2:
-                        g.fig.axes[ax_idx].plot(maxl[names.index(param1)], maxl[names.index(param2)],
-                                                marker='*', ms=10.0, color='k', mew=1.0, mfc='w')
-                    ax_idx += 1
-                finished.append(param1)
-        if save_fn:
-            plt.savefig(save_fn, bbox_inches='tight')
+            for c, chain in enumerate(self.chains):
+                names = chain['names']
+                maxl = chain['samples'][chain['log_likelihood'].argmax()]
+                finished = []
+                ax_idx = 0
+                for i, param1 in enumerate(names):
+                    for j, param2 in enumerate(names[::-1]):
+                        if param2 in finished: continue
+                        if param1 != param2:
+                            g.fig.axes[ax_idx].plot(maxl[names.index(param1)], maxl[names.index(param2)],
+                                                    marker='*', ms=10.0, color=colors[c], mew=1.0, mfc='w')
+                        ax_idx += 1
+                    finished.append(param1)
         
         return g
