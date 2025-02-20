8a28d577402149789497492f74cefc526545cc1a
SBouchard01 <simonbouchard47@gmail.com>
2025-02-20 08:00:57 -0800

    Added controlplots to bgs init imports

--- acm/projects/bgs/__init__.py
+++ acm/projects/bgs/__init__.py
@@ -3,4 +3,5 @@
 from .tpcf import GalaxyCorrelationFunctionMultipoles
 from .dsc_conf import DensitySplitCorrelationFunctionMultipoles
 
+from .control_plots import ControlPlots
 from .priors.priors import Bouchard25, get_priors, truth_within_priors
