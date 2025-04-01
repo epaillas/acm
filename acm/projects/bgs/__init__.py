2ea687e79a85fc6721c203671ccaaf97ed2b5cd9
epaillas <enrique.paillas@uwaterloo.ca>
2025-03-31 21:04:31 -0700

    update

--- acm/projects/bgs/__init__.py
+++
@@ -1,6 +0,0 @@
-
-
-from .tpcf import GalaxyCorrelationFunctionMultipoles
-from .dsc_conf import DensitySplitCorrelationFunctionMultipoles
-
-from .priors.priors import Bouchard25, get_priors, truth_within_priors
