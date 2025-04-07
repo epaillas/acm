112ef547350ecfc74dc019f34ebf6077a1bd9114
epaillas <enrique.paillas@uwaterloo.ca>
2025-04-07 12:31:07 -0700

    fix path

--- acm/observables/emc/minkowski.py
+++ acm/observables/emc/minkowski.py
@@ -70,7 +70,7 @@ class MinkowskiFunctionals(BaseObservable):
     def get_emulator_error(self, select_filters=None, slice_filters=None):
         from pathlib import Path
         import numpy as np
-        data_dir = '/global/homes/e/epaillas/pscratch/emc/v1.1/emulator_error/'
+        data_dir = '/pscratch/sd/e/epaillas/emc/v1.1/emulator_error/'
         data_fn = Path(data_dir) / 'minkowski.npy'
         data = np.load(data_fn, allow_pickle=True).item()
         return data['emulator_error']
