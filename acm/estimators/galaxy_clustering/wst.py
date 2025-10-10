f15da292b322efb7b8bbe05f8e1d7060cf7d72a9
epaillas <enrique.paillas@uwaterloo.ca>
2025-10-10 16:07:54 -0700

    fix to make WST work on CPU and GPU

--- acm/estimators/galaxy_clustering/wst.py
+++ acm/estimators/galaxy_clustering/wst.py
@@ -1,4 +1,5 @@
-from kymatio.jax import HarmonicScattering3D
+import torch
+from kymatio.torch import HarmonicScattering3D
 import numpy as np
 import logging
 import time
@@ -24,6 +25,15 @@ class WaveletScatteringTransform(BaseDensityMeshEstimator):
             max_order=2
         )
 
+        if torch.cuda.is_available():
+            self.device = 'cuda'
+            self.logger.info(f'Using GPU: {torch.cuda.get_device_name(0)}')
+        else:
+            self.device = 'cpu'
+            self.logger.info('Using CPU')
+        self.S.to(self.device)
+        self.integral_powers = integral_powers
+
     def run(self):
         """
         Run the wavelet scattering transform.
@@ -36,12 +46,14 @@ class WaveletScatteringTransform(BaseDensityMeshEstimator):
         t0 = time.time()
         query_positions = self.get_query_positions(self.delta_mesh, method='lattice')
         self.delta_query = self.delta_mesh.read(query_positions).reshape(self.data_mesh.meshsize)
+        # if self.device == 'cuda':
+        self.delta_query = torch.tensor(np.copy(self.delta_query), dtype=torch.float32).to(self.device)
         smat_orders_12 = self.S(self.delta_query)
-        smat = np.absolute(smat_orders_12[:, :, 0])
-        s0 = np.sum(np.absolute(self.delta_mesh)**0.80)
+        smat = torch.absolute(smat_orders_12[:, :, 0])
+        s0 = torch.sum(torch.absolute(self.delta_query)**self.integral_powers[0])
         smatavg = smat.flatten()
-        self.smatavg = np.hstack((s0, smatavg))
-        self.logger.info(f"WST coefficients calculated in {time.time() - t0:.2f} s.")
+        self.smatavg = torch.hstack((s0, smatavg)).cpu()
+        self.logger.info(f"WST coefficients done in {time.time() - t0:.2f} s.")
         return self.smatavg
 
     def plot_coefficients(self, save_fn=None):
