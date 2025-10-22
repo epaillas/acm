aa333b83934d5e06bc0860804f4cfa0774fcd7c3
SBouchard01 <simonbouchard47@gmail.com>
2025-10-22 06:34:03 -0700

    Add get_mesh_attrs for inferring mesh attributes in BaseDensityMeshEstimator

--- acm/estimators/galaxy_clustering/base.py
+++ acm/estimators/galaxy_clustering/base.py
@@ -1,4 +1,4 @@
-from jaxpower import MeshAttrs, ParticleField, FKPField, ComplexMeshField, RealMeshField
+from jaxpower import MeshAttrs, ParticleField, FKPField, ComplexMeshField, RealMeshField, get_mesh_attrs
 from jax import numpy as jnp
 import numpy as np
 import time
@@ -16,14 +16,22 @@ class BaseDensityMeshEstimator(BaseEstimator):
     """
     Base estimator class for environment-based estimators.
     """
-    def __init__(self, data_positions, data_weights=None, randoms_positions=None, randoms_weights=None,  **kwargs):
+    def __init__(self, data_positions, data_weights=None, randoms_positions=None, randoms_weights=None, **kwargs):
         super().__init__()
-        self.mattrs = MeshAttrs(**kwargs)
+        # Set mesh attributes directly if passed, otherwise infer from positions
+        if set(kwargs.keys()).issubset(set(MeshAttrs().keys())): 
+            self.mattrs = MeshAttrs(**kwargs) 
+        else:
+            self.logger.info('Inferring mesh attributes from data and randoms positions.')  
+            pos = [p for p in [data_positions, randoms_positions] if p is not None] # get_mesh_attrs raises an error if randoms_positions is None
+            self.mattrs = get_mesh_attrs(*pos, **kwargs)
+            
         self.data_mesh = ParticleField(data_positions, data_weights, attrs=self.mattrs, exchange=True, backend='jax', out='complex')
         self.randoms_mesh = None
         self.size_data = len(data_positions)
         if randoms_positions is not None:
             self.randoms_mesh = ParticleField(randoms_positions, randoms_weights, attrs=self.mattrs, exchange=True, backend='jax', out='complex')
+        
         self.boxsize = self.data_mesh.boxsize
         self.boxcenter = self.data_mesh.boxcenter
         self.meshsize = self.data_mesh.meshsize
