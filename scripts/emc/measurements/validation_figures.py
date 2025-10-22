35b367533e2a155e0f57ddf4f5b5a9077ec672bb
epaillas <enrique.paillas@uwaterloo.ca>
2025-10-22 10:19:23 -0700

    validated post-recon power spectrum

--- scripts/emc/measurements/validation_figures.py
+++ scripts/emc/measurements/validation_figures.py
@@ -19,16 +19,22 @@ def get_spectrum_fns(cosmo_idx=0, phase_idx=0, n_hod=1, sim_type='base'):
         fns = sorted(base_dir.glob(handle))
         return fns
 
-def get_recon_spectrum_fns(cosmo_idx=0, phase_idx=0, n_hod=1):
-    base_dir = Path(f'/pscratch/sd/e/epaillas/emc/v1.2/abacus/raw_measurements/recon_spectrum/c{cosmo_idx:03}_ph000/seed0/')
-    handle = f'mesh2_recon_spectrum_poles_c{cosmo_idx:03}_hod???.h5'
-    fns = sorted(base_dir.glob(handle))
-    return fns[:n_hod]
+def get_recon_spectrum_fns(cosmo_idx=0, phase_idx=0, n_hod=1, sim_type='base'):
+    if sim_type == 'base':
+        base_dir = Path(f'/pscratch/sd/e/epaillas/emc/v1.2/abacus/raw_measurements/recon_spectrum/c{cosmo_idx:03}_ph000/seed0/')
+        handle = f'mesh2_recon_spectrum_poles_c{cosmo_idx:03}_hod???.h5'
+        fns = sorted(base_dir.glob(handle))
+        return fns[:n_hod]
+    else:  # small
+        base_dir = Path(f'/pscratch/sd/e/epaillas/emc/v1.2/abacus/small/recon_spectrum/')
+        handle = f'mesh2_recon_spectrum_poles_ph*.h5'
+        fns = sorted(base_dir.glob(handle))
+        return fns
 
 def read_spectrum(filename):
     data = read(filename)
-    kmin, kmax = 0.01, 0.7
-    data = data.select(k=slice(0, None, 5)).select(k=(kmin, kmax))
+    kmin, kmax = 0.0126, 0.7
+    data = data.select(k=slice(0, None, 13)).select(k=(kmin, kmax))
     poles = [data.get(ell) for ell in (0, 2, 4)]
     k = poles[0].coords('k')
     return k, poles
@@ -104,6 +110,32 @@ def plot_recon_spectrum():
         ax[ell//2].set_ylabel(r'$k P_{%d}(k)$ [h$^{-2}$ Mpc$^{2}$]' % ell, fontsize=13)
     plt.tight_layout()
     plt.savefig('fig/recon_spectrum_measurements.png', dpi=300, bbox_inches='tight')
+    plt.close()
+
+def plot_recon_spectrum_small():
+    pk_fns = get_recon_spectrum_fns(sim_type='small')[:100]
+    k, _ = read_spectrum(pk_fns[0])
+    poles = [read_spectrum(fn)[1] for fn in pk_fns]
+    print(k)
+    print(np.diff(k))
+    print(np.diff(k).mean())
+    print(len(k))
+    print(np.array(poles[0][1]))
+    fig, ax = plt.subplots(1, 3, figsize=(11, 3))
+    for pole in poles[:50]:
+        for ell in (0, 2, 4):
+            ax[ell//2].plot(k, k*pole[ell//2], ls='-', lw=0.5, color='gray')
+    # plot mean
+    mean_poles = np.mean(np.array([[pole[ell//2] for ell in (0, 2, 4)] for pole in poles]), axis=0)
+    std_poles = np.std(np.array([[pole[ell//2] for ell in (0, 2, 4)] for pole in poles]), axis=0) / 5
+    for ell in (0, 2, 4):
+        ax[ell//2].errorbar(k, k*mean_poles[ell//2], yerr=k*std_poles[ell//2], fmt='o', markersize=1, capsize=0.5, elinewidth=1.0, label='Mean', color='k')
+    for aa in ax:
+        aa.set_xlabel(r'$k$ [h/Mpc]', fontsize=13)
+    for ell in (0, 2, 4):
+        ax[ell//2].set_ylabel(r'$k P_{%d}(k)$ [h$^{-2}$ Mpc$^{2}$]' % ell, fontsize=13)
+    plt.tight_layout()
+    plt.savefig('fig/recon_spectrum_measurements_small.png', dpi=300, bbox_inches='tight')
     plt.close()
 
 def compare_spectrum_vs_recon_spectrum():
@@ -192,3 +224,4 @@ if __name__ == '__main__':
     plot_recon_tpcf()
     compare_tpcf_vs_recon_tpcf()
     plot_spectrum_small()
+    plot_recon_spectrum_small()
