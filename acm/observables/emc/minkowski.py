960d304c1017cf533f55d12b092ede669f798c49
epaillas <enrique.paillas@uwaterloo.ca>
2025-10-29 16:19:02 -0700

    simplify parsing of hod index from filename

--- acm/observables/emc/minkowski.py
+++ acm/observables/emc/minkowski.py
@@ -22,16 +22,11 @@ class MinkowskiFunctionals(BaseObservableEMC):
         """
         Override checkpoint_fn to point to the correct checkpoint file.
         """
-        return '/pscratch/sd/e/epaillas/emc/v1.2/trained_models/best/spectrum/last.ckpt'
+        return '/pscratch/sd/e/epaillas/emc/v1.2/trained_models/best/minkowski/last.ckpt'
     
     def compress_covariance(
         self,
         save_to: str = None,
-        kmin: float = 0.01,
-        kmax: float = 0.7, 
-        rebin: int = 5,
-        ells: list = [0, 2, 4],
-        overwrite_k: np.ndarray = None
     ) -> xarray.DataArray:
         """
         Compress the covariance array from the raw measurement files.
@@ -41,51 +36,44 @@ class MinkowskiFunctionals(BaseObservableEMC):
         save_to : str
             Path of the directory where to save the compressed covariance and bin_values. If None, it is not saved.
             Default is None.
-        kmin : float
-            Minimum k value to consider. Default is 0.01.
-        kmax : float
-            Maximum k value to consider. Default is 0.7.
-        rebin : int
-            Rebinning factor for the statistics. Default is 4.
-        ells : list
-            List of multipoles to compute the statistics for. Default is [0, 2, 4].
-        overwrite_k : np.ndarray
-            If not None, overwrite the final separation values with this array. 
-            This is primarily useful to ensure consistency between the covariance and the data dims.
-            Default is None.
             
         Returns
         -------
         xarray.DataArray
             Covariance array. 
         """
-        from jaxpower import read
         # Directories
         base_dir = Path(self.paths['measurements_dir']) / 'small' / self.stat_name
-        data_fns = list(base_dir.glob('mesh2_spectrum_poles_ph*.h5')) # NOTE: File name format hardcoded !
-        
+        data_fns = list(base_dir.glob('minkowski_ph*.npy')) # NOTE: File name format hardcoded !
+
+        threshold_index = np.load(
+            '/pscratch/sd/e/epaillas/emc/Threshold_index_for_MFs_with_Rg5_7_10_15.npy',
+            allow_pickle=True
+        ).item()
+
         y = []
-        for data_fn in data_fns:
-            data = read(data_fn)
-            data = data.select(k=slice(0, None, rebin)).select(k=(kmin, kmax))
-            poles = [data.get(ell) for ell in (0, 2, 4)]
-            k = poles[0].coords('k')
-            y.append(np.concatenate(poles))
+        for filename in data_fns:
+            self.logger.info(f'Compressing {filename}')
+            data = np.load(filename, allow_pickle=True).item()
+            mf = []
+            for i in [5, 7, 10, 15]:
+                Rg = f'Rg{i}'
+                for j in range(4):
+                    mf.append(data[Rg][threshold_index[f'Threshold_index_{Rg}'][j], j ] * (10 * i) ** j) 
+            y.append(np.concatenate(mf))
         y = np.array(y)
-        k = overwrite_k if overwrite_k is not None else k
-        
+
         self.logger.info(f'Loaded covariance with shape: {y.shape}')
         
         cout = xarray.DataArray(
-            data = y.reshape(y.shape[0], len(ells), -1),
+            data = y.reshape(y.shape[0], -1),
             coords = {
                 "phase_idx": list(range(y.shape[0])),
-                "multipoles": ells,
-                "k": k,
+                'bin_idx': list(range(y.shape[-1])),
             },
             attrs = {
                 "sample": ["phase_idx"],
-                "features": ["multipoles", "k"],
+                "features": ["bin_idx"],
             },
             name = "covariance_y",
         )
@@ -100,10 +88,6 @@ class MinkowskiFunctionals(BaseObservableEMC):
         self, 
         add_covariance: bool = False,
         save_to: str = None,
-        kmin: float = 0.01,
-        kmax: float = 0.7, 
-        rebin: int = 5,
-        ells: list = [0, 2, 4],
         cosmos: list = cosmo_list,
         n_hod: int = 500,
         phase_idx: int = 0,
@@ -119,14 +103,6 @@ class MinkowskiFunctionals(BaseObservableEMC):
         save_to : str
             Path of the directory where to save the compressed file. If None, it is not saved.
             Default is None.
-        kmin : float
-            Minimum k value to consider. Default is 0.01.
-        kmax : float
-            Maximum k value to consider. Default is 0.7.
-        rebin : int
-            Rebinning factor for the statistics. Default is 4.
-        ells : list
-            List of multipoles to compute the statistics for. Default is [0, 2, 4].
         cosmos : list
             List of cosmological parameters to use. If None, use all cosmological parameters.
             Default is None.
@@ -143,7 +119,6 @@ class MinkowskiFunctionals(BaseObservableEMC):
             Compressed dataset containing 'x' and 'y' DataArrays. 
             If add_covariance is True, also contains 'covariance_y' DataArray.
         """
-        from jaxpower import read
         base_dir = Path(self.paths['measurements_dir'],  f'base/{self.stat_name}/')
         
         threshold_index = np.load(
@@ -166,9 +141,10 @@ class MinkowskiFunctionals(BaseObservableEMC):
                     for j in range(4):
                         mf.append(data[Rg][threshold_index[f'Threshold_index_{Rg}'][j], j ] * (10 * i) ** j) 
                 y.append(np.concatenate(mf))
-                hod_idx = int(str(filename).split('hod')[1].split('.')[0])
+                hod_idx = int(filename.stem.split('hod')[-1])
                 hods[cosmo_idx].append(hod_idx)
-            self.logger.info(f'HOD indices: {hods[cosmo_idx]}')
+            # self.logger.info(f'HOD indices: {hods[cosmo_idx]}')
+            self.logger.info(f'Number of HODs for c{cosmo_idx:03}: {len(hods[cosmo_idx])}')
         y = np.array(y)
         
         y = xarray.DataArray(
@@ -195,7 +171,7 @@ class MinkowskiFunctionals(BaseObservableEMC):
             },
         )
         if add_covariance:
-            cov_y = self.compress_covariance(rebin=rebin, ells=ells, overwrite_k=k)
+            cov_y = self.compress_covariance()
             cout = xarray.merge([cout, cov_y])
         
         if save_to is not None:
@@ -268,3 +244,63 @@ class MinkowskiFunctionals(BaseObservableEMC):
             Corrected predictions.
         """
         return (1 + prediction) * (1 + self.phase_correction) - 1
+
+    def plot(self, model_params: dict, save_fn: str = None):
+        """
+        Plot multi-scale Minkowski functionals predictions against data.
+
+        Parameters
+        ----------
+        model_params : dict
+            Dictionary of model parameters to use for the prediction.
+        save_fn : str
+            Filename to save the plot. If None, the plot is not saved.
+
+        Returns
+        -------
+        matplotlib.figure.Figure
+            The generated plot figure.
+        """
+        import matplotlib.pyplot as plt
+        plt.rcParams.update({
+            "text.usetex": True,
+            "font.family": "serif",
+            "font.serif": ["Computer Modern Roman"],
+        })
+
+        height_ratios = [3, 1]
+        figsize = (6, 1.5 * sum(height_ratios))
+        fig, lax = plt.subplots(len(height_ratios), sharex=True, sharey=False,
+            gridspec_kw={'height_ratios': height_ratios}, figsize=figsize, squeeze=True)
+        fig.subplots_adjust(hspace=0.1)
+        show_legend = False
+
+        lax[-1].set_xlabel(r'$\textrm{bin index}$]', fontsize=15)
+        lax[0].set_ylabel(r'$\textrm{Minkowski functionals}$', fontsize=15)
+
+        bin_idx = self.bin_idx
+        data = self.y[0]
+        model = self.get_model_prediction(model_params)[0]
+        cov = self.get_covariance_matrix(volume_factor=64)
+        error = np.sqrt(np.diag(cov))
+
+        lax[0].errorbar(bin_idx, data, error, marker='o', ms=3, ls='', 
+            color=f'C0', elinewidth=1.0, capsize=None)
+        lax[0].plot(bin_idx, model, ls='-', color=f'C1')
+        lax[1].plot(bin_idx, (data - model) / error, ls='-', color=f'C0')
+
+        for offset in [-2, 2]: lax[1].axhline(offset, color='k', ls='--')
+        lax[1].set_ylabel(r'$\Delta \textrm{MF} / \sigma_\textrm{MF}$', fontsize=15)
+        lax[1].set_ylim(-4, 4)
+
+        for ax in lax:
+            ax.grid(True)
+            ax.tick_params(axis='both', labelsize=14)
+        if show_legend: lax[0].legend(fontsize=15)
+
+        if save_fn is not None:
+            plt.savefig(save_fn, dpi=300, bbox_inches='tight')
+            self.logger.info(f'Saving plot to {save_fn}')
+        return fig
+
+
