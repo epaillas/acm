ad304050276952d50715133c739fff030acf45e4
epaillas <enrique.paillas@uwaterloo.ca>
2025-10-29 17:38:26 -0700

    which statistics to compress into files now as a command-line argument

--- scripts/emc/measurements/compress_files.py
+++ scripts/emc/measurements/compress_files.py
@@ -1,21 +1,21 @@
 import acm.observables.emc as emc
 from acm import setup_logging
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Compress EMC measurement files.')
+parser.add_argument('--stats', nargs='+', default=['spectrum'], help='List of statistics to compress.')
+args = parser.parse_args()
+todo_stats = args.stats
 
 setup_logging()
-
-todo_stats = [
-    # 'spectrum',
-    # 'bispectrum',
-    # 'recon_spectrum',
-    # 'minkowski',
-    'projected_tpcf',
-]
 
 paths = {
     'data_dir': '/pscratch/sd/e/epaillas/emc/v1.2/abacus/compressed/',
     'measurements_dir': '/pscratch/sd/e/epaillas/emc/v1.2/abacus/',
     'param_dir': '/pscratch/sd/e/epaillas/emc/cosmo+hod_params/',
 }
+
 
 for stat in todo_stats:
     if stat == 'spectrum':
@@ -24,8 +24,7 @@ for stat in todo_stats:
 
     if stat == 'bispectrum':
         observable = emc.GalaxyBispectrumMultipoles(paths=paths)
-        observable.compress_data(save_to=paths['data_dir'], add_covariance=True, n_hod=9)
-        # observable.compress_covariance(save_to=paths['data_dir'])
+        observable.compress_data(save_to=paths['data_dir'], add_covariance=True, n_hod=30)
 
     if stat == 'recon_spectrum':
         observable = emc.ReconstructedGalaxyPowerSpectrumMultipoles(paths=paths)
@@ -37,4 +36,4 @@ for stat in todo_stats:
 
     if stat == 'projected_tpcf':
         observable = emc.ProjectedGalaxyCorrelationFunction(paths=paths)
-        observable.compress_data(save_to=paths['data_dir'], add_covariance=True, n_hod=200)
+        observable.compress_data(save_to=paths['data_dir'], add_covariance=True, n_hod=500)
