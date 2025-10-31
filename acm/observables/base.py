97062a75d72c89ec29a8c6bc273857281f633166
SBouchard01 <simonbouchard47@gmail.com>
2025-10-31 10:13:34 -0700

    Clarified parameter descriptions in get_model_prediction for Observable and BaseObservableEMC classes

--- acm/observables/base.py
+++ acm/observables/base.py
@@ -401,14 +401,16 @@ class Observable():
             model.transform_input = WeiLiuInputTransform()
         return model
     
-    def get_model_prediction(self, x, model=None, coords=None, attrs=None, nofilters: bool = False) -> xarray.DataArray:
+    def get_model_prediction(self, x, model=None, coords: dict = None, attrs: dict = None, nofilters: bool = False):
         """
         Get the prediction from the model.
         
         Parameters
         ----------
         x : array_like, dict
-            Input features.
+            Input features for the model. 
+            If an array, it should have shape (n_samples, n_params). 
+            If a dict, it should have keys matching the model input names and values as lists/1d-arrays of shape (n_samples,).
         model : FCN
             Trained theory model. If None, the model attribute of the class is used. Defaults to None.
         coords : dict, optional
@@ -437,7 +439,7 @@ class Observable():
                     f"Unexpected keys: {extra}"
                 )
             x = [x[name] for name in self.x_names]
-            x = np.asarray(x).T  # Need to transpose to (n_samples, n_features)
+            x = np.asarray(x).T  # Need to transpose to (n_samples, n_params)
         else:
             x = np.asarray(x)  # Ensure x is an array to make torch.Tensor faster
         
