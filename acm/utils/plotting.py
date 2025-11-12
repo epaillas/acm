6a55c6b308dd16f26f4cb9319f14d14914eca27a
SBouchard01 <simonbouchard47@gmail.com>
2025-11-12 06:01:15 -0800

    add `functools.wraps`  to `set_plot_style`

--- acm/utils/plotting.py
+++ acm/utils/plotting.py
@@ -1,8 +1,9 @@
+from functools import wraps
 import matplotlib.pyplot as plt
-
 
 # decorator to set plotting style
 def set_plot_style(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         plt.rc('text', usetex=True)
         plt.rc('font', family='serif')
