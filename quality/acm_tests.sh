2cf12b43604800c354989331c5a1b8cf939cb1cb
Jean-Marc Colley <jcolley@login04.chn.perlmutter.nersc.gov>
2025-11-21 04:20:23 -0800

    adapt path

--- quality/acm_tests.sh
+++ quality/acm_tests.sh
@@ -4,7 +4,7 @@ cd $ACM_root
 coverage erase
 rm -f quality/report_coverage*
 rm -rf quality/html_cov*
-coverage run --source=acm -m pytest tests/unit -v
-coverage xml -i -o quality/report_coverage.xml
+coverage run --source=acm -m pytest tests -v
+#coverage xml -i -o quality/report_coverage.xml
 coverage html -i -d quality/html_coverage
 coverage report
