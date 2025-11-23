0dd17cdad86cb4ac7ab883e8eac0c0afa72ad580
Paul Demidov <pd2487@login10.chn.perlmutter.nersc.gov>
2025-11-23 07:49:39 -0800

    knn measurements

--- acm/observables/emc/__init__.py
+++ acm/observables/emc/__init__.py
@@ -3,5 +3,6 @@ from .spectrum import GalaxyPowerSpectrumMultipoles
 from .recon_spectrum import ReconstructedGalaxyPowerSpectrumMultipoles
 from .density_split_correlation import DensitySplitCorrelationFunctionMultipoles
 from .minkowski import MinkowskiFunctionals
+from .dd_knn import DDkNN
 
 from .priors.priors import get_priors
