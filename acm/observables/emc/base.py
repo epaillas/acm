3057346da08b18ba893c4412dafb0a6c0762d76c
SBouchard01 <simonbouchard47@gmail.com>
2025-12-04 23:54:40 -0800

    Rename get_raw_hod_idx to get_hod_from_files and update data directory path; refactor compress_x to improve HOD handling and add error checks

--- acm/observables/emc/base.py
+++ acm/observables/emc/base.py
@@ -221,7 +221,7 @@ class BaseObservableEMC(Observable):
             pred = pred.values
         return pred
 
-    def get_raw_hod_idx(self, cosmo_idx: int, phase: int = 0, seed: int = 0) -> np.ndarray:
+    def get_hod_from_files(self, cosmo_idx: int, phase: int = 0, seed: int = 0) -> np.ndarray:
         """
         Get the HOD indexes from the statistic files for a given phase and seed.
         
@@ -241,50 +241,87 @@ class BaseObservableEMC(Observable):
         np.ndarray
             Array of HOD indexes.
         """
-        data_dir = '/pscratch/sd/n/ntbfin/emulator/hods/z0.5/yuan23_prior'
+        data_dir = self.paths['hod_dir']
         data_dir = Path(data_dir) / f'c{cosmo_idx:03d}_ph{phase:03d}' / f'seed{seed}'
         hod_idx = [int(fn.stem.lstrip('hod')) for fn in sorted(data_dir.glob('hod*'))] 
         return np.array(hod_idx)
         
-    def compress_x(self, hods: dict, cosmos: list = cosmo_list) -> tuple:
+    def compress_x(
+        self,  
+        cosmos: list = cosmo_list,
+        n_hod: int = None,
+        **kwargs
+    ) -> xarray.DataArray:
         """
         Compress the x values from the parameters files.
         
         Parameters
         ----------
-        hods : dict
-            Dictionary of hods for each cosmology.
         cosmos : list, optional
             List of cosmologies to get from the files. The default is None, which means all cosmologies.
-        
+        n_hod : int, optional
+            Number of HODs to consider per cosmology.
+            If None, it is determined from the first cosmology and restricted to that number for all cosmologies. 
+            Defaults to None.
+        kwargs : dict
+            Additional arguments to pass to the get_hod_from_files method.
+            
         Returns
         -------
         xarray.DataArray
             Compressed x values.
+            
+        Raises
+        ------
+        ValueError
+            If the number of HODs for a cosmology is lower than the expected number, 
+            as the compression requires all cosmologies to have the same number of HODs.
         """
-        data_dir = self.paths['param_dir']
-
-        filename = '/pscratch/sd/e/epaillas/emc/AbacusSummit.csv'
-        cosmo_params = load_abacus_cosmologies(filename, cosmos, ['omega_b', 'omega_cdm', 'sigma8_m', 'n_s', 'alpha_s', 'N_ur', 'w0_fld', 'wa_fld'], mapping={'alpha_s': 'nrun'})
+        # NOTE: Hardcoded paths :/
+        cosmo_file = '/pscratch/sd/e/epaillas/emc/AbacusSummit.csv' 
+        hod_file = '/pscratch/sd/n/ntbfin/emulator/hods/hod_params.npy'
         
-        n_hod = len(hods[cosmo_list[0]])  # assumes same number of hods for each cosmology
+        cosmo_params = load_abacus_cosmologies(
+            cosmo_file, 
+            cosmologies = cosmos, 
+            parameters = ['omega_b', 'omega_cdm', 'sigma8_m', 'n_s', 'alpha_s', 'N_ur', 'w0_fld', 'wa_fld'], 
+            mapping={'alpha_s': 'nrun'},
+        )
+        hod_params = np.load(hod_file, allow_pickle=True).item()
+        
         x = []
-        x_hods = np.load('/pscratch/sd/n/ntbfin/emulator/hods/hod_params.npy', allow_pickle=True).item()
         for cosmo_idx in cosmos:
-            x_hod = x_hods[f'c{cosmo_idx:03}']
-            x_hod_names = list(x_hod.keys())  # Store the HOD parameter names only once
-            x_hod = np.array([x_hod[param] for param in x_hod.keys()]).T
-            x_hod = x_hod[hods[cosmo_idx]]
-            x_cosmo = [cosmo_params[f'c{cosmo_idx:03}'][param] for param in cosmo_params[f'c{cosmo_idx:03}'].keys()]
-            x_cosmo = np.repeat(np.array(x_cosmo).reshape(1, -1), n_hod, axis=0)
-            x.append(np.concatenate([x_cosmo, x_hod], axis=1))
-            x_cosmo_names = [name for name in cosmo_params[f'c{cosmo_idx:03}'].keys()]
+            # HOD parameters
+            x_hod = hod_params[f'c{cosmo_idx:03}']
+            x_hod_names = list(x_hod.keys())
+            x_hod = np.array([x_hod[param] for param in x_hod.keys()]).T # Make x_hod into an array
+            
+            # Cosmo parameters
+            x_cosmo = cosmo_params[f'c{cosmo_idx:03}']
+            x_cosmo_names = list(x_cosmo.keys())
+            x_cosmo = np.array([x_cosmo[param] for param in x_cosmo_names])
+            x_cosmo = np.repeat(x_cosmo.reshape(1, -1), x_hod.shape[0], axis=0)
+            
+            # Full parameters
+            x_i = np.concatenate([x_cosmo, x_hod], axis=1)
+            
+            hod_idx = self.get_hod_from_files(cosmo_idx, **kwargs)
+            if n_hod is None:
+                n_hod = len(hod_idx) # Determine the number of HODs from the first cosmology
+                self.logger.info(f'Number of HODs determined for c{cosmo_idx:03d}: {n_hod}')
+            
+            # Ensure the number of HODs is as expected
+            if len(hod_idx) > n_hod:
+                hod_idx = hod_idx[:n_hod] # Restrict to the expected number of HODs
+                self.logger.info(f'Number of HODs for c{cosmo_idx:03d} is larger than expected ({len(hod_idx)} > {n_hod}). Restricting to the first {n_hod} HODs.')
+            elif len(hod_idx) < n_hod:
+                raise ValueError(f'Number of HODs for c{cosmo_idx:03d} is lower than expected ({len(hod_idx)} < {n_hod}). Cannot proceed with compression.')
+            
+            x.append(x_i[hod_idx, :])
+            
         x = np.concatenate(x)
         x_names = x_cosmo_names + x_hod_names
-        order = ['omega_b', 'omega_cdm', 'sigma8_m', 'n_s', 'nrun', 'N_ur', 'w0_fld', 'wa_fld', 'logM_cut', 'logM_1', 'sigma', 'alpha', 'kappa', 'alpha_c', 'alpha_s', 's', 'A_cen', 'A_sat', 'B_cen', 'B_sat']
-        idx_sorted = [np.where(np.array(x_names) == name)[0][0] for name in order]
-        x = x[:, idx_sorted]
-        x_names = [x_names[i] for i in idx_sorted]
+        
         x = xarray.DataArray(
             x.reshape(len(cosmos), n_hod, -1),
             coords = {
