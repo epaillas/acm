6176e319ed8ec9f268cb8e95cf6635fef92b3374
epaillas <enrique.paillas@uwaterloo.ca>
2025-12-15 12:05:11 -0800

    rework wst estimator after fixing meshsize

--- acm/estimators/galaxy_clustering/wst.py
+++ acm/estimators/galaxy_clustering/wst.py
@@ -10,16 +10,15 @@ class WaveletScatteringTransform(BaseDensityMeshEstimator):
     """
     Class to compute the wavelet scattering transform.
     """
-    def __init__(self, J_3d=4, L_3d=4, integral_powers=[0.8], sigma=0.8, init_kymatio=None, **kwargs):
+    def __init__(self, J=4, L=4, q=0.8, sigma=0.8, init_kymatio=None, **kwargs):
 
         self.logger = logging.getLogger('WaveletScatteringTransform')
-        self.logger.info('Initializing WaveletScatteringTransform.')
         super().__init__(**kwargs)
 
-        self.J_3d = J_3d
-        self.L_3d = L_3d
+        self.J = J
+        self.L = L
         self.sigma_0 = sigma
-        self.integral_powers = integral_powers
+        self.q = q
         self.max_order = 2
 
         self.query_positions = self.get_query_positions(method='lattice')
@@ -34,9 +33,11 @@ class WaveletScatteringTransform(BaseDensityMeshEstimator):
         """
         Initialize the kymatio scattering transform.
         """
+        self.logger.info('Initializing WaveletScatteringTransform.')
+        self.logger.info(f'J={self.J}, L={self.L}, sigma_0={self.sigma_0}, max_order={self.max_order}')
         self.S = HarmonicScattering3D(
-            J=self.J_3d,
-            L=self.L_3d,
+            J=self.J,
+            L=self.L,
             shape=self.meshsize,
             max_order=self.max_order,
             sigma_0=self.sigma_0,
@@ -59,7 +60,7 @@ class WaveletScatteringTransform(BaseDensityMeshEstimator):
             self.delta_query = self.delta_mesh.read(self.query_positions).reshape(self.meshsize)
         smat_orders_12 = self.S(self.delta_query)
         smat = np.absolute(smat_orders_12[:, :, 0])
-        s0 = np.sum(np.absolute(self.delta_query)**self.integral_powers[0])
+        s0 = np.sum(np.absolute(self.delta_query) ** self.q)
         smatavg = smat.flatten()
         self.smatavg = np.hstack((s0, smatavg))
         self.smatavg /= np.prod(self.meshsize)
