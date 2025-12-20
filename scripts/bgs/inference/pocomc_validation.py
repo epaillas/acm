d08ff9ac7cec0680a803f1d0e6b266c2e2616ef0
SBouchard01 <simonbouchard47@gmail.com>
2025-12-19 17:49:15 -0800

    Adding final validation & secondgen inference scripts

--- scripts/bgs/inference/pocomc_validation.py
+++ scripts/bgs/inference/pocomc_validation.py
@@ -1,3 +1,5 @@
+import sys
+import yaml
 import logging
 import argparse
 import numpy as np
@@ -6,10 +8,11 @@ from utils import get_fixed_params # Will be moved to sunbird
 from sunbird.inference.pocomc import PocoMCSampler
 from sunbird.inference.priors import get_priors, Bouchard25
 from acm import setup_logging
-from acm.utils.covariance import get_covariance_correction
+from acm.utils.covariance import get_covariance_correction, check_covariance_matrix
 from utils import get_observable, save_and_plot
+from secondgen_bgs import get_secondgen_data
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__file__.split('/')[-1])
 
 # Define select filters map for different observables
 select_filters_map = {
@@ -26,21 +29,45 @@ select_filters_map = {
     ),
 }
 
-# Define functions for inference
-def get_data_model_cov(
-    observable, 
-    add_cov_emu: bool = False, 
-    cov_emu_method: str = 'median', 
-    cov_emu_diag: bool = False, 
-    cov_correction: str = 'percival'
-) -> tuple:
+def get_observable_model(observable) -> tuple:
     """
-    Load data, model, and covariance matrix for the given observable.
+    Load model for the given observable.
 
     Parameters
     ----------
     observable : CombinedObservable
         The observable object containing data and methods to get covariance and model predictions.
+
+    Returns
+    -------
+    model : callable
+        The theory model prediction function.
+    x_names : list
+        Names of the input data parameters.
+    """
+    x_names = observable.x_names
+    model = observable.get_model_prediction # This is a method passed to the sampler !
+    return model, x_names
+
+def get_observable_covariance(
+    observable, 
+    *args, # See PEP 3102
+    add_cov_emu: bool = False, 
+    cov_emu_method: str = 'median', 
+    cov_emu_diag: bool = False, 
+    cov_correction: str = 'percival', 
+    fixed_parameters: list[str] = None,
+) -> np.ndarray:
+    """
+    Load covariance matrix for the given observable, adds emulator covariance if specified,
+    and applies covariance correction if specified.
+
+    Parameters
+    ----------
+    observable : CombinedObservable
+        The observable object containing data and methods to get covariance and model predictions.
+    *args :
+        Additional covariances to add to the total covariance matrix.
     add_cov_emu : bool, optional
         Whether to add emulator covariance to the total covariance matrix. Defaults to False.
     cov_emu_method : str, optional
@@ -49,28 +76,14 @@ def get_data_model_cov(
         Whether to use only the diagonal of the emulator covariance. Defaults to False.
     cov_correction : str, optional
         Covariance correction method to use. Defaults to 'percival'.
+    fixed_parameters : list[str], optional
+        List of fixed parameter names for covariance correction. Defaults to None.
 
     Returns
     -------
-    data_x : np.ndarray
-        The input data (e.g., cosmo and hod parameters).
-    data_x_names : list
-        Names of the input data parameters.
-    data_y : np.ndarray
-        The observed data (e.g., clustering measurements).
-    model : callable
-        The theory model prediction function.
     cov : np.ndarray
         The total covariance matrix.
     """
-    x = observable.x
-    x_names = observable.x_names
-    y = observable.y
-
-    logger.info(f'Loaded data_x with shape: {x.shape}')
-    logger.info(f'Loaded data_y with shape {y.shape}')
-
-    # load the covariance matrix, including emulator error and Percival correction
     cov = observable.get_covariance_matrix(volume_factor=64)
     logger.info(f'Loaded covariance matrix with shape: {cov.shape}')
     
@@ -79,20 +92,46 @@ def get_data_model_cov(
         logger.info(f'Loaded emulator covariance matrix with shape: {cov_emu.shape}')
         cov += cov_emu
     
+    if args: 
+        for additional_cov in args:
+            cov += additional_cov
+            
     if cov_correction is not None:
         correction = get_covariance_correction(
             method = cov_correction, 
             n_s = len(observable.covariance_y),
             n_d = len(cov),
-            n_theta = len(x_names) - len(fixed_param_names),
+            n_theta = len(observable.x_names) - len(fixed_parameters),
         )
         logger.info(f'Applying covariance correction: {correction}')
         cov *= correction
-        
-    # Load the theory model
-    model = observable.get_model_prediction # This is a method passed to the sampler !
     
-    return x, x_names, y, model, cov
+    logger.info(f'Final covariance matrix tests (warnings will follow if any fail):')
+    check_covariance_matrix(cov, name='Total covariance matrix')
+    
+    return cov
+
+def get_observable_data(observable) -> tuple: 
+    """
+    Load observable data (x, y) from the given observable.
+
+    Parameters
+    ----------
+    observable : CombinedObservable
+        The observable object containing data.
+
+    Returns
+    -------
+    x : np.ndarray
+        The input data (e.g., cosmo and hod parameters).
+    y : np.ndarray
+        The observed data (e.g., clustering measurements).
+    """
+    x = observable.x
+    y = observable.y
+    logger.info(f'Loaded observable x with shape: {x.shape}')
+    logger.info(f'Loaded observable y with shape: {y.shape}')
+    return x, y
 
 def fit_pocomc(
     observable, 
@@ -100,6 +139,7 @@ def fit_pocomc(
     ranges: dict, 
     labels: dict, 
     fixed_param_names: list[str],
+    fit_type: str = 'validation',
     add_cov_emu: bool = False,
     cov_emu_method: str = 'median',
     cov_emu_diag: bool = False,
@@ -134,12 +174,25 @@ def fit_pocomc(
     sampler : PocoMCSampler
         The configured PocoMCSampler instance after sampling.
     """
-    x, x_names, observation, theory_model, cov = get_data_model_cov(
+    theory_model, x_names = get_observable_model(observable, )
+    
+    covariances = []
+    if fit_type == 'validation':
+        x, observation = get_observable_data(observable)
+    elif fit_type == 'secondgen':
+        x, observation, extra_cov = get_secondgen_data(observable)
+        covariances.append(extra_cov)
+    else:
+        raise ValueError(f'Unknown fit_type: {fit_type}')
+    
+    cov = get_observable_covariance(
         observable, 
+        *covariances,
         add_cov_emu=add_cov_emu, 
         cov_emu_method=cov_emu_method, 
         cov_emu_diag=cov_emu_diag,
         cov_correction=cov_correction,
+        fixed_parameters=fixed_param_names,
     )
     
     # Ensure prior keys are in the same order as data_names
@@ -168,20 +221,40 @@ def fit_pocomc(
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser()
+    parser.add_argument('--config', type=str, default=None, help='Path to a configuration file (YAML format). Command line arguments override config file settings.')
+    parser.add_argument('--dump_config', action='store_true', help='Dump the current configuration and exit.')
     parser.add_argument('--module', type=str, default='acm.observables.bgs', help='Base module path for observables')
     parser.add_argument('--statistics', nargs='+', help='List of statistics to use, e.g. GalaxyPowerSpectrumMultipoles')
-    parser.add_argument("--cosmo_idx", type=int, default=0)
-    parser.add_argument("--hod_idx", type=int, default=0)
-    parser.add_argument('--add_cov_emu', action='store_true', help='Whether to add emulator covariance or not.')
-    parser.add_argument('--cosmo_model', type=str, default='base', help='Cosmological model to use.')
-    parser.add_argument('--hod_model', type=str, default='base-VB-AB-CB-s', help='HOD model to use.')
-    parser.add_argument('--identifier', type=str, default=None, help='Identifier for the run.')
+    parser.add_argument('-t', '--fit_type', type=str, default='validation', help='Type of mock to fit: validation or secondgen.')
+    parser.add_argument('-ce', '--add_cov_emu', action='store_true', help='Whether to add emulator covariance or not.')
+    parser.add_argument('-cm', '--cosmo_model', type=str, default='base', help='Cosmological model to use.')
+    parser.add_argument('-hm', '--hod_model', type=str, default='base-VB-AB-CB-s', help='HOD model to use.')
+    parser.add_argument('-id', '--identifier', type=str, default=None, help='Identifier for the run.')
+    parser.add_argument('-d', '--cov_emu_diag', action='store_true', help='Whether to use only the diagonal of the emulator covariance.')
     parser.add_argument('--cov_emu_method', type=str, default='median', help='Method to compute the emulator covariance.')
-    parser.add_argument('--cov_emu_diag', action='store_true', help='Whether to use only the diagonal of the emulator covariance.')
     parser.add_argument('--cov_correction', type=str, default='percival', help='Covariance correction method to use.')
-    parser.add_argument('--save_dir', type=str, default='/pscratch/sd/s/sbouchar/acm/bgs/chains/validation/')
+    parser.add_argument('--save_dir', type=str, default=None, help='Directory to save results.')
+    parser.add_argument('--cosmo_idx', type=int, default=0, help='Index of the cosmology to fit, if fit_type is validation.')
+    parser.add_argument('--hod_idx', type=int, default=0, help='Index of the HOD to fit, if fit_type is validation.')
     
     args = parser.parse_args()
+    
+    if args.config is not None:
+        with open(args.config, 'r') as f:
+            config_args = yaml.safe_load(f)
+            parser.set_defaults(**config_args)
+        args = parser.parse_args() # Re-parse arguments with config defaults
+    if args.dump_config:
+        parser.print_help(sys.stdout)
+        print("\nCurrent configuration:")
+        print("----------------------")
+        tmp_args = vars(args).copy()
+        del tmp_args['config']
+        del tmp_args['dump_config']
+        for arg in tmp_args:
+            print(f"{arg}: {getattr(args, arg)}")
+        sys.exit(-1)
+        
     setup_logging()
     
     module = args.module
@@ -189,6 +262,7 @@ if __name__ == '__main__':
     cosmo_model = args.cosmo_model
     hod_model = args.hod_model
     identifier = args.identifier
+    fit_type = args.fit_type
     
     logger.info(f'Running inference for cosmo model: {cosmo_model}, hod model: {hod_model}')
     
@@ -210,16 +284,21 @@ if __name__ == '__main__':
         ranges, 
         labels, 
         fixed_param_names,
+        fit_type=fit_type,
         add_cov_emu=args.add_cov_emu,
         cov_emu_method=args.cov_emu_method,
         cov_emu_diag=args.cov_emu_diag,
         cov_correction=args.cov_correction,
     )
     
-    save_dir = Path(args.save_dir) / f'c{args.cosmo_idx:03d}_h{args.hod_idx:03d}' / f'cosmo-{cosmo_model}_hod-{hod_model}'
+    if fit_type == 'validation':
+        save_dir = Path(args.save_dir) / fit_type / f'c{args.cosmo_idx:03d}_h{args.hod_idx:03d}' / f'cosmo-{cosmo_model}_hod-{hod_model}'
+    else:
+        save_dir = Path(args.save_dir) / fit_type / f'cosmo-{cosmo_model}_hod-{hod_model}' # No indices for usual mocks fits
     
     save_and_plot(
         sampler, 
         observable, 
+        identifier=identifier,
         save_dir=save_dir,
     )
