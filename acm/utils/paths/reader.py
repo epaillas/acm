37f1b9e7bb66bae7049fcb2378475f01438da2b0
SBouchard01 <simonbouchard47@gmail.com>
2026-01-22 21:21:06 +0100

    add list_path_files and remove extension dependency in resolve_yaml_path

--- acm/utils/paths/reader.py
+++ acm/utils/paths/reader.py
@@ -1,19 +1,24 @@
 import yaml
 from pathlib import Path
+from typing import Callable, Any
 from acm.utils.decorators import require_nersc
 
 @require_nersc(enabled=True)
-def resolve_yaml_path(filename: str, *keys: str):
+def resolve_yaml_path(
+    filename: str, 
+    *keys: str,
+    loader: Callable = yaml.safe_load,
+) -> Any:
     """
-    Reads a .yaml file and parses the arguments as nested keys to return the corresponding value.
+    Reads a file and parses the arguments as nested keys to return the corresponding value.
 
     Parameters
     ----------
     filename : str
-        The path to the .yaml file to read. Relative paths will be resolved as relative to the script's location, 
+        The path to the file to read. Relative paths will be resolved as relative to the script's location, 
         with a fallback to the acm.utils.paths directory.
     *keys : str
-        Nested keys to traverse the YAML structure.
+        Nested keys to traverse the file structure.
     
     Returns
     -------
@@ -23,21 +28,18 @@ def resolve_yaml_path(filename: str, *keys: str):
     Raises
     ------
     FileNotFoundError
-        If the specified YAML file does not exist.
+        If the specified file does not exist.
     ValueError
-        If the provided keys do not lead to a valid value in the YAML structure.
+        If the provided keys do not lead to a valid value in the structure.
     """
     here = Path(__file__).parent
     fn = Path(filename) # Assert absolute or relative to cwd
     
     if not fn.exists():
-        fn = here / filename # Relative to this script
-        
-    if not fn.exists():
-        raise FileNotFoundError(f"YAML file not found: {filename}")
+        fn = here / filename # Relative to this script's directory
 
     with open(fn, 'r') as file:
-        data = yaml.safe_load(file)
+        data = loader(file)
         
         for key in keys:
             if not isinstance(data, dict) or key not in data:
@@ -45,3 +47,24 @@ def resolve_yaml_path(filename: str, *keys: str):
             data = data.get(key)
 
     return data
+
+def list_path_files(
+    ext: tuple[str, ...] = (".yaml", ".yml"),
+    recursive: bool = False,
+) -> list[str]:
+    """
+    Lists all available paths files shipped with this package
+
+    Returns
+    -------
+    list[str]
+        A list of filenames available in the specified directory.
+    """
+    base_dir = Path(__file__).parent
+    patterns = [f"**/*{e}" if recursive else f"*{e}" for e in ext]
+    files = []
+    
+    for pattern in patterns:
+        files.extend([f.name for f in base_dir.glob(pattern)])
+    
+    return files
