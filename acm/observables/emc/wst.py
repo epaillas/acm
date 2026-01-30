7fd81bf9119d39f14a7ae7f35a1906bba73267c2
SBouchard01 <simonbouchrd47@gmail.com>
2026-01-31 00:47:49 +0100

    add wst alias

--- acm/observables/emc/wst.py
+++ acm/observables/emc/wst.py
@@ -321,3 +321,6 @@ class WaveletScatteringTransform(BaseObservableEMC):
             self.logger.info(f'Saving training set figure to {save_fn}')
             
         return fig, ax
+
+# Alias
+wst = WaveletScatteringTransform
