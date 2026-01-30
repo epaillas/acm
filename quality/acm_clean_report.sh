479d10b096f0276130ef9b82e6657180c53e9b2c
luckyjim <jcolley@lpnhe.in2p3.fr>
2026-01-30 06:26:07 -0800

    test only tests/acm

--- quality/acm_clean_report.sh
+++ quality/acm_clean_report.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 cd $ACM_root
-rm -rf quality/html_*
-rm -rf quality/report_*
+rm -rf quality/*_report*
 rm .coverage
 
--- quality/acm_tests.sh
+++ quality/acm_tests.sh
@@ -5,7 +5,7 @@ coverage erase
 # xml is for SonarQub
 #rm -f quality/coverage_report.xml
 rm -rf quality/coverage_report_html
-coverage run --source=acm -m pytest tests -v
+coverage run --source=acm -m pytest tests/acm -vv
 #coverage xml -i -o quality/coverage_report.xml
 coverage html -i -d quality/coverage_report_html
 coverage report
