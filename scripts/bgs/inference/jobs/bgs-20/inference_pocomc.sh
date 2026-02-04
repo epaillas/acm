2d61c5d8fb4e3bdf8db190380c8168e3c723e484
SBouchard01 <simonbouchard47@gmail.com>
2026-02-04 15:01:45 +0100

    Move inference jobs to dedicated folder

---
+++ scripts/bgs/inference/jobs/bgs-20/inference_pocomc.sh
@@ -0,0 +1,41 @@
+#!/bin/bash -l
+
+#SBATCH --account desi
+#SBATCH --nodes 1
+#SBATCH --ntasks-per-node 1
+#SBATCH --qos regular
+#SBATCH --constraint cpu
+
+#SBATCH --time 04:00:00
+
+#SBATCH --job-name s10-sg-basew0wa
+#SBATCH --output /pscratch/sd/s/sbouchar/Output_jobs/bgs-20_inference/secondgen/%A.%x_%a.out
+#SBATCH --error /pscratch/sd/s/sbouchar/Output_jobs/bgs-20_inference/secondgen/%A.%x_%a.err
+
+# Load the modules of the DESI environment (cosmodesi)
+source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
+
+cd /global/homes/s/sbouchar/acm-repo/scripts/bgs/inference/
+
+COSMO_MODEL=base-w0wa
+CONFIG=jobs/bgs-20/config_secondgen.yaml
+
+S_MIN=10
+S_MAX=200
+SLICE_MAP="{'tpcf': {'s': [$S_MIN, $S_MAX]}, 'ds_xiqg': {'s': [$S_MIN, $S_MAX]}, 'ds_xigg': {'s': [$S_MIN, $S_MAX]}}"
+# SLICE_MAP='None' # No slicing by default
+
+# HOD_MODEL=None
+# python inference_pocomc.py --config $CONFIG --statistics tpcf --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL --slice_map "$SLICE_MAP"
+# python inference_pocomc.py --config $CONFIG --statistics ds_xiqg ds_xiqq --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL --slice_map "$SLICE_MAP"
+# python inference_pocomc.py --config $CONFIG --statistics tpcf ds_xiqg ds_xiqq --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL --slice_map "$SLICE_MAP"
+
+# HOD_MODEL=base
+# python inference_pocomc.py --config $CONFIG --statistics tpcf --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL --slice_map "$SLICE_MAP"
+# python inference_pocomc.py --config $CONFIG --statistics ds_xiqg ds_xiqq --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL --slice_map "$SLICE_MAP"
+# python inference_pocomc.py --config $CONFIG --statistics tpcf ds_xiqg ds_xiqq --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL --slice_map "$SLICE_MAP"
+
+HOD_MODEL=base-AB-CB-VB-s
+python inference_pocomc.py --config $CONFIG --statistics tpcf --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL --slice_map "$SLICE_MAP"
+python inference_pocomc.py --config $CONFIG --statistics ds_xiqg ds_xiqq --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL --slice_map "$SLICE_MAP"
+python inference_pocomc.py --config $CONFIG --statistics tpcf ds_xiqg ds_xiqq --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL --slice_map "$SLICE_MAP"
