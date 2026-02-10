710dafbf6e6fcfd53cc5e67c594faca492682428
SBouchard01 <simonbouchard47@gmail.com>
2026-02-10 06:48:51 -0800

    Update optimize_model scripts for BGS: adjust paths and handle error due to get instead of getattr used in Observable call

--- scripts/bgs/training/optimize_model.sh
+++ scripts/bgs/training/optimize_model.sh
@@ -9,16 +9,16 @@
 #SBATCH --time 48:00:00
 
 #SBATCH --job-name tpcf_train
-#SBATCH --output /pscratch/sd/s/sbouchar/Output_jobs/bgs_training/%A.%x_%a.out
-#SBATCH --error /pscratch/sd/s/sbouchar/Output_jobs/bgs_training/%A.%x_%a.err
+#SBATCH --output /pscratch/sd/s/sbouchar/Output_jobs/bgs-21.35_training/%A.%x_%a.out
+#SBATCH --error /pscratch/sd/s/sbouchar/Output_jobs/bgs-21.35_training/%A.%x_%a.err
 
 # Load the modules of the DESI environment (cosmodesi)
 source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 
-python /global/homes/s/sbouchar/acm/scripts/bgs/training/optimize_model.py \
-    --compressed_dir /pscratch/sd/s/sbouchar/acm/bgs-20/input_data \
-    --study_dir /pscratch/sd/s/sbouchar/acm/bgs-20/trained_models/sigma_clipped/study \
-    --save_dir /pscratch/sd/s/sbouchar/acm/bgs-20/trained_models/sigma_clipped \
+python /global/homes/s/sbouchar/acm-repo/scripts/bgs/training/optimize_model.py \
+    --compressed_dir /pscratch/sd/s/sbouchar/acm/bgs-21.35/input_data \
+    --study_dir /pscratch/sd/s/sbouchar/acm/bgs-21.35/trained_models/study \
+    --save_dir /pscratch/sd/s/sbouchar/acm/bgs-21.35/trained_models \
     --n_trials 100 \
     --transform arcsinh \
     --same_n_hidden \
