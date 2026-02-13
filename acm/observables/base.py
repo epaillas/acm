ac958139291679505a7049c077ef95ae06bc6ce2
epaillas <enrique.paillas@uwaterloo.ca>
2026-02-13 07:29:38 -0800

    print outliers in emulator residual plot

--- acm/observables/base.py
+++ acm/observables/base.py
@@ -824,7 +824,9 @@ class Observable():
 
             ax[1].plot(emu_err / data_err, lw=1.0, label=method)
 
-            self.logger.info(f"Emulator residuals are larger than 10 sigma in bins: {np.where(emu_err / data_err > 10)[0].tolist()} using method '{method}'.")
+            outliers = np.where(emu_err / data_err > 10)[0].tolist()
+            if len(outliers) > 0:
+                self.logger.info(f"Emulator residuals are larger than 10 sigma in bins: {outliers} using method '{method}'.")
 
         ax[1].axhline(1.0, color='k', ls=':', lw=0.7)
         ax[1].set_xlabel('bin index', fontsize=13)
