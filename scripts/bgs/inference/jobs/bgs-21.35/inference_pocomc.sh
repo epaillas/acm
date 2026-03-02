f6c6abae707efb1a97addfafe136341fac40a73e
SBouchard01 <simonbouchard47@gmail.com>
2026-03-02 02:05:14 -0800

    Update inference control plots & jobs with better syntax

--- scripts/bgs/inference/jobs/bgs-21.35/inference_pocomc.sh
+++ scripts/bgs/inference/jobs/bgs-21.35/inference_pocomc.sh
@@ -6,9 +6,9 @@
 #SBATCH --qos regular
 #SBATCH --constraint cpu
 
-#SBATCH --time 03:00:00
+#SBATCH --time 10:00:00
 
-#SBATCH --job-name 21-val-inference
+#SBATCH --job-name val21-inference
 #SBATCH --output /pscratch/sd/s/sbouchar/Output_jobs/bgs-21.35_inference/validation/%A.%x_%a.out
 #SBATCH --error /pscratch/sd/s/sbouchar/Output_jobs/bgs-21.35_inference/validation/%A.%x_%a.err
 
@@ -22,31 +22,22 @@ HOD_MODEL=base-AB-CB-VB-s
 
 SLICE_MAP="{'tpcf': {'s': [30, 150]}, 'ds_xiqg': {'s': [1, 30]}, 'ds_xiqq': {'s': [1, 30]}}"
 
-COSMO_MODEL=base-w0wa-fixed-omega_b
-python inference_pocomc.py --config $CONFIG --statistics tpcf --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL
-python inference_pocomc.py --config $CONFIG --statistics ds_xiqg ds_xiqq --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL
-python inference_pocomc.py --config $CONFIG --statistics tpcf ds_xiqg ds_xiqq --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL
+COSMO_MODEL_LIST=(
+    # base-w0wa-fixed-omega_b
+    base-fixed-omega_b
+    base-w0wa
+    base
+    base-nrun-Nur-w0wa
+    # base-nrun-Nur-w0wa-fixed-omega_b
+)
 
-python inference_pocomc.py --config $CONFIG --statistics tpcf --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL --slice_map "$SLICE_MAP"
+for COSMO_MODEL in "${COSMO_MODEL_LIST[@]}"; do
+    echo "Running inference for COSMO_MODEL: $COSMO_MODEL"
 
-COSMO_MODEL=base-fixed-omega_b
-python inference_pocomc.py --config $CONFIG --statistics tpcf --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL
-python inference_pocomc.py --config $CONFIG --statistics ds_xiqg ds_xiqq --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL
-python inference_pocomc.py --config $CONFIG --statistics tpcf ds_xiqg ds_xiqq --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL
-
-python inference_pocomc.py --config $CONFIG --statistics tpcf --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL --slice_map "$SLICE_MAP"
-
-# COSMO_MODEL=base-w0wa
-# python inference_pocomc.py --config $CONFIG --statistics tpcf --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL
-# python inference_pocomc.py --config $CONFIG --statistics ds_xiqg ds_xiqq --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL
-# python inference_pocomc.py --config $CONFIG --statistics tpcf ds_xiqg ds_xiqq --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL
-
-# python inference_pocomc.py --config $CONFIG --statistics tpcf --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL --slice_map "$SLICE_MAP"
-
-# COSMO_MODEL=base
-# python inference_pocomc.py --config $CONFIG --statistics tpcf --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL
-# python inference_pocomc.py --config $CONFIG --statistics ds_xiqg ds_xiqq --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL
-# python inference_pocomc.py --config $CONFIG --statistics tpcf ds_xiqg ds_xiqq --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL
-
-# python inference_pocomc.py --config $CONFIG --statistics tpcf --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL --slice_map "$SLICE_MAP"
+    python inference_pocomc.py --config $CONFIG --statistics tpcf --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL
+    python inference_pocomc.py --config $CONFIG --statistics ds_xiqg ds_xiqq --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL
+    python inference_pocomc.py --config $CONFIG --statistics tpcf ds_xiqg ds_xiqq --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL
+    
+    python inference_pocomc.py --config $CONFIG --statistics tpcf --cosmo_model $COSMO_MODEL --hod_model $HOD_MODEL --slice_map "$SLICE_MAP"
+done
 
