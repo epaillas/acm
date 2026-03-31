916af698cc82e33e2c3277c9d77a874ccc8c654b
epaillas <epaillas@arizona.edu>
2026-03-31 14:06:34 -0700

    Rename EMC observable modules

--- acm/observables/emc/__init__.py
+++ acm/observables/emc/__init__.py
@@ -1,12 +1,34 @@
-from .tpcf import tpcf
-from .projected_tpcf import projected_tpcf
-from .spectrum import spectrum
-from .bispectrum import bispectrum
-from .recon_spectrum import recon_spectrum
-from .minkowski import minkowski
-from .dd_knn import dd_knn
-from .density_split_correlation import ds_xiqg, ds_xiqq
-from .vide import vide_ccf, vide_vsf
-from .wst import wst
-from .versus import sv_vsf, sv_recon_vsf, sv_xivg, sv_recon_xivg, sv_xivv, sv_recon_xivv
-from .pdf import pdf
+from .tpcf_module import tpcf
+from .projected_tpcf_module import projected_tpcf
+from .spectrum_module import spectrum
+from .bispectrum_module import bispectrum
+from .recon_spectrum_module import recon_spectrum
+from .minkowski_module import minkowski
+from .dd_knn_module import dd_knn
+from .density_split_module import ds_xiqg, ds_xiqq
+from .vide_module import vide_ccf, vide_vsf
+from .wst_module import wst
+from .versus_module import sv_vsf, sv_recon_vsf, sv_xivg, sv_recon_xivg, sv_xivv, sv_recon_xivv
+from .pdf_module import pdf
+
+__all__ = [
+    "tpcf",
+    "projected_tpcf",
+    "spectrum",
+    "bispectrum",
+    "recon_spectrum",
+    "minkowski",
+    "dd_knn",
+    "ds_xiqg",
+    "ds_xiqq",
+    "vide_ccf",
+    "vide_vsf",
+    "wst",
+    "sv_vsf",
+    "sv_recon_vsf",
+    "sv_xivg",
+    "sv_recon_xivg",
+    "sv_xivv",
+    "sv_recon_xivv",
+    "pdf",
+]
