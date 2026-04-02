81a15a4ec3bbb4fefc9b1a6127eebb6e26fb3fa3
SBouchard01 <simonbouchard47@gmail.com>
2026-04-02 09:15:54 -0700

    Update outliers script

--- scripts/bgs/measurements/outliers.py
+++ scripts/bgs/measurements/outliers.py
@@ -3,13 +3,16 @@ Identify outliers in measurements using sigma clipping.
 Note that this requires all the HOD folders to have all their measurements computed for the expected mocks (otherwise the compression functions will crash).
 
 Usage:
-    python outliers.py --sim_type small -md /pscratch/sd/s/sbouchar/acm/bgs-20/measurements -pd /pscratch/sd/s/sbouchar/acm/bgs-20/parameters/cosmo+hod_params --measurements tpcf ds_xiqg ds_xiqq --ells 0 2 --seed 0 --sigma 6.0 --log_level info
+    python outliers.py --sim_type small -md /pscratch/sd/s/sbouchar/acm/bgs-20/measurements -pd /pscratch/sd/s/sbouchar/acm/bgs-20/parameters/cosmo+hod_params --compress_args "{ells: [0, 2], seed: 0}" --measurements tpcf ds_xiqg ds_xiqq --sigma 6.0 --log_level info 
+
+    python outliers.py --sim_type base -md /pscratch/sd/s/sbouchar/acm/bgs-20/measurements --method h5_corruption --measurements power_spectrum ds_pkqg ds_pkqq --log_level info
 """
 
 import logging
 import argparse
 from pathlib import Path
 
+import lsstypes
 import numpy as np
 from astropy.stats import sigma_clip
 
@@ -113,12 +116,11 @@ def get_outliers(data: np.ndarray, index: np.ndarray = None, **kwargs):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Identify outliers in TPCF measurements using sigma clipping.")
     parser.add_argument('--module', type=str, default='acm.observables.bgs', help='Base module path for observables')
+    parser.add_argument('--method', type=str, default='sigma_clip', help='Method to identify outliers. "h5_corruption" identifies outliers based on corrupted h5 files, while "sigma_clip" uses sigma clipping on the data.')
     parser.add_argument('--measurements_dir', '-md', type=str, default=None, required=True, help='Directory containing measurement files')
-    parser.add_argument('--param_dir', '-pd', type=str, default=None, required=True, help='Directory containing HOD parameter files')
+    parser.add_argument('--param_dir', '-pd', type=str, default=None, help='Directory containing HOD parameter files')
     parser.add_argument('--sim_type', type=str, choices=['small', 'base'], default='small', help='Type of simulation to analyze.')
-    parser.add_argument('--ells', type=int, nargs='+', default=[0, 2], help='Multipoles to consider.')
-    parser.add_argument('--seed', type=int, default=0, help='Seed number for measurements.')
-    parser.add_argument('--hods', type=int, nargs='+', default=[157], help='HOD indexes covariance for measurements.')
+    parser.add_argument('--compress_args', type=str, default='{}', help=r'Additional keyword arguments to pass to the compression functions, as a stringified dictionary. E.g., "{ells: [0, 2], seed: 0}"')
     parser.add_argument('--sigma', type=float, default=6.0, help='Sigma threshold for clipping.')
     parser.add_argument('--measurements', type=str, nargs='+', default='tpcf', help='Type of measurements to analyze.')
     parser.add_argument('--save_dir', type=str, help='Directory to save the outlier results to a file.')
@@ -126,15 +128,15 @@ if __name__ == "__main__":
     args = parser.parse_args()
     
     sim_type = args.sim_type
-    ells = args.ells
     sigma = args.sigma
+    compress_args = {}.update(eval(args.compress_args)) # Convert stringified dict to actual dict
     
     logger = logging.getLogger(__file__.split('/')[-1])
     setup_logging(level=args.log_level)
     
     logger.info(f'Using {sim_type} boxes')
-    logger.info(f'Using multipoles: {ells}')
     logger.info(f'Using sigma clipping threshold: {sigma}')
+    logger.info(f'Using method: {args.method}')
     
     paths = dict(
         measurements_dir = args.measurements_dir,
@@ -144,22 +146,61 @@ if __name__ == "__main__":
     outliers_dict_all = {} # To store outliers for all statistics if saving
     
     for stat_name in args.measurements:
-        cls = get_class_from_module(args.module, stat_name)
         
-        # set compression args for specific statistics if needed
-        compress_args = dict(seed=args.seed)
-        if stat_name in ['ds_xiqg', 'ds_xiqq']:
-            compress_args['quantiles'] = [0, 1, 2, 3, 4]
+        # For the h5 corruption method, we simply check which files cannot be read as valid h5 files, and consider those as outliers. 
+        # This is a quick way to identify corrupted files without needing to load all the data into memory.
+        if args.method == 'h5_corruption':
+            n_outliers = 0
+            outliers_dict = {}
+            
+            data_dir = Path(args.measurements_dir) / sim_type
+            
+            h5_files = list(data_dir.glob(f'*/*/*/{stat_name}*.h5')) # Assuming the structure is measurements_dir/sim_type/c000_ph000/seed0/hod000/stat_name*.h5
+            
+            for fn in h5_files:
+                try:
+                    lsstypes.read(fn)
+                except OSError as e:
+                    logger.warning(f'File {fn} is corrupted and cannot be read as a valid h5 file. Marking as outlier.')
+                    
+                    # Extract cosmo, phase, seed and hod from the file path
+                    parts = fn.parts
+                    cosmo = parts[-4].split('_')[0].lstrip('c') # e.g., 000
+                    phase = parts[-4].split('_')[1].lstrip('ph') # e.g., 000
+                    seed = parts[-3].lstrip('seed') # e.g., 0
+                    hod = parts[-2].lstrip('hod') # e.g., 000
+                    
+                    # Update the outliers dictionary with the corrupted file information
+                    nested_set(outliers_dict, [cosmo, phase, seed], hod, extend=True)
+                    nested_set(outliers_dict_all, [cosmo, phase, seed], hod, extend=True)
+                    
+                    n_outliers += 1
+            
+            if args.save_dir and n_outliers > 0:
+                outlier_dir = Path(args.save_dir)
+                outlier_dir.mkdir(exist_ok=True)
+                        
+                outlier_fn = outlier_dir / f'{stat_name}_outliers_simtype-{sim_type}_h5_corruption.npy'
+                np.save(outlier_fn, outliers_dict)
+                logger.info(f'Saved {n_outliers} {stat_name} outliers to {outlier_fn}')
+                
+                outlier_fn_all = outlier_dir / f'all_outliers_simtype-{sim_type}_h5_corruption.npy'
+                np.save(outlier_fn_all, outliers_dict_all)
+                logger.info(f'Saved all outliers to {outlier_fn_all}')
         
-        outliers_dict = {}
-        
-        for hod in args.hods:
-            print(f'Analyzing {stat_name} for HOD index {hod}')
+        elif args.method == 'sigma_clip':
+            cls = get_class_from_module(args.module, stat_name)
+            
+            # Update compression args for specific statistics if needed 
+            if stat_name in ['ds_xiqg', 'ds_xiqq']:
+                compress_args['quantiles'] = [0, 1, 2, 3, 4]
+            
+            outliers_dict = {}
             
             if sim_type == 'small':
-                index, data = get_covariance(cls, paths=paths, ells=ells, hod_idx=hod, **compress_args)
+                index, data = get_covariance(cls, paths=paths, **compress_args)
             elif sim_type == 'base':
-                index, data = get_y(cls, paths=paths, ells=ells, **compress_args)
+                index, data = get_y(cls, paths=paths, **compress_args)
             else: 
                 raise ValueError(f"sim_type must be one of ['base', 'small']")
             
@@ -192,10 +233,13 @@ if __name__ == "__main__":
                 outlier_dir = Path(args.save_dir)
                 outlier_dir.mkdir(exist_ok=True)
                 
-                outlier_fn = outlier_dir / f'{stat_name}_outliers_simtype-{sim_type}_ells-{"".join(map(str, ells))}_sigma-{sigma}.npy'
+                outlier_fn = outlier_dir / f'{stat_name}_outliers_simtype-{sim_type}_sigma-{sigma}.npy'
                 np.save(outlier_fn, outliers_dict)
                 logger.info(f'Saved {n_outliers} {stat_name} outliers to {outlier_fn}')
                 
-                outlier_fn_all = outlier_dir / f'all_outliers_simtype-{sim_type}_ells-{"".join(map(str, ells))}_sigma-{sigma}.npy'
+                outlier_fn_all = outlier_dir / f'all_outliers_simtype-{sim_type}_sigma-{sigma}.npy'
                 np.save(outlier_fn_all, outliers_dict_all)
                 logger.info(f'Saved all outliers to {outlier_fn_all}')
+        
+        else:
+            raise ValueError(f"Method must be one of ['h5_corruption', 'sigma_clip']")
