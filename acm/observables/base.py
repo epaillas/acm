7332a9281fd3e50e6cf8f80287fdbfe5d375cbc0
Ruff formatter <formatter@ruff.org>
2026-04-09 22:42:42 +0000

    Ruff format code and import

--- acm/observables/base.py
+++ acm/observables/base.py
@@ -1,6 +1,3 @@
-from acm.utils.plotting import set_plot_style
-from acm.utils.xarray import dataset_from_dict
-
 import logging
 from copy import copy, deepcopy
 from pathlib import Path
@@ -17,6 +14,8 @@ from sunbird.emulators import FCN
 from acm.utils.covariance import check_covariance_matrix, orthogonal_gk_mad_covariance
 from acm.utils.decorators import temporary_class_state
 from acm.utils.logging import supress_logging
+from acm.utils.plotting import set_plot_style
+from acm.utils.xarray import dataset_from_dict
 
 # Register safe globals for transform classes to allow loading checkpoints
 # with PyTorch 2.6+ (which changed weights_only default to True)
