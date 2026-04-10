5e531ac14bfc39afc503359d3bf3bdb808b316cb
SBouchard01 <simonbouchrd47@gmail.com>
2026-04-10 18:35:08 +0200

    Enhance versioning workflows: add no-bump job and update check-version-label triggers
    Also add the __version__ attribute to acm from the package metadata

--- acm/__init__.py
+++ acm/__init__.py
@@ -1,1 +1,7 @@
 from .utils.logging import setup_logging
+
+from importlib.metadata import version, PackageNotFoundError
+try:
+    __version__ = version("acm")
+except PackageNotFoundError:
+    __version__ = "unknown"
