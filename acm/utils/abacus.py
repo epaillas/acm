f65e95ad45f8a7004acb54217662764835736626
SBouchard01 <simonbouchard47@gmail.com>
2026-06-10 20:06:24 +0200

    feat: update load_abacus_cosmologies to accept Path or str for filename and improve parameter handling + add tests

--- acm/utils/abacus.py
+++ acm/utils/abacus.py
@@ -8,7 +8,7 @@ logger = logging.getLogger(__name__)
 
 
 def load_abacus_cosmologies(
-    filename: str,
+    filename: Path | str,
     cosmologies: list[int],
     parameters: list[str],
     mapping: dict[str, str] | None = None,
@@ -20,7 +20,7 @@ def load_abacus_cosmologies(
 
     Parameters
     ----------
-    filename : str
+    filename : Path | str
         Filename (csv) with the AbacusSummit cosmology parameters.
     cosmologies : list[int]
         List of cosmologies indexes to select.
@@ -34,15 +34,18 @@ def load_abacus_cosmologies(
     dict
         Dictionary with the selected cosmology parameters for the selected cosmologies.
     """
-    cosmo_params = pd.read_csv(
-        filename,
-        usecols=["root", *parameters],
-    )
-    cosmo_params = cosmo_params[
-        cosmo_params["root"].isin([f"abacus_cosm{c:03d}" for c in cosmologies])
-    ]
-    cosmo_params = cosmo_params.drop(columns=["root"])
-    cosmo_params = cosmo_params.set_index(pd.Index([f"c{c:03d}" for c in cosmologies]))
+    filename = Path(filename)  # Ensure filename is a Path object
+    csv = pd.read_csv(filename, usecols=["root", *parameters])
+    
+    root = csv["root"]
+    params = csv[parameters]
+    
+    cnames = [f"abacus_cosm{c:03d}" for c in cosmologies] # cosmology names to select
+    index = pd.Index([f"c{c:03d}" for c in cosmologies]) # New cosmology indexes
+
+    cosmo_params = params[root.isin(cnames)]
+    if not cosmo_params.empty:
+        cosmo_params = cosmo_params.set_index(index)
     if mapping is not None:
         cosmo_params = cosmo_params.rename(columns=mapping)
     return cosmo_params.to_dict(orient="index")
@@ -73,18 +76,21 @@ def get_abacus_phases(
         A tuple containing a list of file paths and a list of phase indices.
     """
     phase_dir = Path(phase_dir)  # Ensure phase_dir is a Path object
+    
+    if not phase_dir.is_dir() or not phase_dir.exists():
+        raise ValueError(f"Provided phase_dir {phase_dir} is not a valid directory.")
 
-    glob_pattern = f"AbacusSummit_small_c{cosmo:03d}_ph*/*/z{z:.3f}/"
-    abacus_fns = sorted(phase_dir.glob(glob_pattern))
-
+    # Patterns (NOTE: hardcoded structure !)
     z_str = f"{z:.3f}".replace(".", r"\.")  # Convert z to a string suitable for regex
-    re_pattern = re.compile(
-        rf"AbacusSummit_small_c{cosmo:03d}_ph(?P<phase>\d+)\/.+\/z{z_str}"
-    )
-
+    re_expr = rf"AbacusSummit_small_c{cosmo:03d}_ph(?P<phase>\d+)\/.+\/z{z_str}"
+    glob_pattern = f"AbacusSummit_small_c{cosmo:03d}_ph*/*/z{z:.3f}/"
+    
+    re_pattern = re.compile(re_expr)
+    fns = sorted(phase_dir.glob(glob_pattern))
+    
     phases = []
-    for f in abacus_fns:
-        match = re_pattern.search(str(f))
+    for f in fns:
+        match = re_pattern.search(str(f.as_posix()))
         if match:
             phases.append(int(match.group("phase")))
         else:
@@ -92,5 +98,4 @@ def get_abacus_phases(
                 f"File {f} does not match the expected pattern and will be skipped."
             )
 
-    fns = [Path(phase_dir) / fn for fn in abacus_fns]
     return fns, phases
