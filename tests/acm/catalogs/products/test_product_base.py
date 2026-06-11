d4fbcc642c72ff434201bf13a6e8bad7253141ef
SBouchard01 <simonbouchrd47@gmail.com>
2026-06-12 00:49:54 +0200

    fix: one test failed - need to ensure all other behave as expected and extend with new calls

--- tests/acm/catalogs/products/test_product_base.py
+++ tests/acm/catalogs/products/test_product_base.py
@@ -119,7 +119,7 @@ class TestTracers:
         pd.testing.assert_frame_equal(result, valid_data)
 
     def test_get_tracer_data_missing_raises(self, catalog):
-        with pytest.raises(KeyError, match="FOO"):
+        with pytest.raises(RuntimeError, match="FOO"):
             catalog.get_tracer_data("FOO")
 
 class TestTransforms:
