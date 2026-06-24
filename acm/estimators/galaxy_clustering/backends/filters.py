28214970c8fc71da04eb38f209e18b03f7f339af
SBouchard01 <simonbouchard47@gmail.com>
2026-06-24 18:02:11 +0200

    feat: refactor filter classes to inherit from BaseFilter and enhance documentation Add NoFilter class (i.e. 1.0 kernel)

--- acm/estimators/galaxy_clustering/backends/filters.py
+++ acm/estimators/galaxy_clustering/backends/filters.py
@@ -1,26 +1,45 @@
 """Legacy module for pypower filters."""
 
+from abc import ABC, abstractmethod
+
 import numpy as np
 
 
-class TopHatFilter:
+class BaseFilter(ABC):
+    """Base class for filters in Fourier space."""
+
+    def __init__(self, r: float) -> None:
+        """
+        Initialize the filter.
+
+        Parameters
+        ----------
+        r : float
+            The smoothing scale (radius) of the filter in Mpc/h.
+        """
+        self.r = r
+
+    @abstractmethod
+    def __call__(self, k: tuple, v: np.ndarray) -> np.ndarray:
+        """
+        Apply the filter.
+
+        Parameters
+        ----------
+        k : tuple of arrays
+            Wavenumber components.
+        v : np.ndarray
+            Field values in Fourier space.
+        """
+        ...
+
+class TopHatFilter(BaseFilter):
     """
     Top-hat filter in Fourier space.
 
     Implements a top-hat filter that can be applied to mesh fields in
     Fourier space. Adapted from https://github.com/bccp/nbodykit/.
     """
-
-    def __init__(self, r: float) -> None:
-        """
-        Initialize the TopHat filter.
-
-        Parameters
-        ----------
-        r : float
-            The radius of the top-hat filter in Mpc/h.
-        """
-        self.r = r
 
     def __call__(self, k: tuple, v: np.ndarray) -> np.ndarray:
         """
@@ -46,25 +65,13 @@ class TopHatFilter:
         w[k == 0] = 1.0
         return w * v
 
-
-class GaussianFilter:
+class GaussianFilter(BaseFilter):
     """
     Gaussian filter in Fourier space.
 
     Implements a Gaussian smoothing filter that can be applied to mesh
     fields in Fourier space.
     """
-
-    def __init__(self, r: float) -> None:
-        """
-        Initialize the Gaussian filter.
-
-        Parameters
-        ----------
-        r : float
-            The smoothing scale (radius) of the Gaussian filter in Mpc/h.
-        """
-        self.r = r
 
     def __call__(self, k: tuple, v: np.ndarray) -> np.ndarray:
         """
@@ -85,3 +92,14 @@ class GaussianFilter:
         r = self.r
         k2 = sum(ki**2 for ki in k)
         return np.exp(-0.5 * k2 * r**2) * v
+
+class NoFilter(BaseFilter):
+    """
+    Class that does not applies a filter.
+
+    Implements a call function that returns the initial object.
+    """
+
+    def __call__(self, k: tuple, v: np.ndarray) -> np.ndarray:  # noqa: ARG002
+        """Don't apply the filter."""
+        return v
