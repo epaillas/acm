5f533e96e639b54135eede2ef23d04d00d743136
SBouchard01 <simonbouchard47@gmail.com>
2026-07-06 14:49:19 +0200

    fix: refactor data field handling in power spectrum computation

--- acm/estimators/galaxy_clustering/spectrum.py
+++ acm/estimators/galaxy_clustering/spectrum.py
@@ -84,22 +84,21 @@ class PowerSpectrumMultipoles(BaseEstimator):
         mattrs = self.backend.mattrs
         bin_mesh = BinMesh2SpectrumPoles(mattrs, edges, ells)
 
-        data_mesh = self.backend.data_field.paint(out="real", **kwargs)
+        data_field = self.backend.data_field
 
         if self.backend.randoms_field is not None:
             # <=> randoms_positions is not None but also checks if randoms_field is not None
             logger.info("Computing power spectrum using FKP estimator with randoms.")
             los = "firstpoint"  # NOTE: override los to firstpoint when using randoms
-            randoms_mesh = self.backend.randoms_field.paint(out="real", **kwargs)
-            fkp = FKPField(data_mesh, randoms_mesh)
+            fkp = FKPField(data_field, self.backend.randoms_field)
             norm = compute_fkp2_normalization(fkp, bin=bin_mesh)
             num_shotnoise = compute_fkp2_shotnoise(fkp, bin=bin_mesh)
             delta_mesh = fkp.paint(out="real", **kwargs)
         else:
             logger.info("Computing power spectrum using box normalization (no randoms)")
-            norm = compute_box2_normalization(data_mesh, bin=bin_mesh)
-            num_shotnoise = compute_fkp2_shotnoise(data_mesh, bin=bin_mesh)
-            delta_mesh = data_mesh.paint(out="real", **kwargs)
+            norm = compute_box2_normalization(data_field, bin=bin_mesh)
+            num_shotnoise = compute_fkp2_shotnoise(data_field, bin=bin_mesh)
+            delta_mesh = data_field.paint(out="real", **kwargs)
             delta_mesh = delta_mesh - delta_mesh.mean()
 
         spectrum = self.jit_cm2s(delta_mesh, bin=bin_mesh, los=los)
@@ -150,5 +149,5 @@ class PowerSpectrumMultipoles(BaseEstimator):
         k = obj.flatten(level=None)[0].coords("k")
         for ell in ells:
             pole = obj.get(ells=ell).value()
-            ax.plot(k, pole * k**2, label=rf"\ell={ell}", **kwargs)
+            ax.plot(k, pole * k**2, label=rf"$\ell={ell}$", **kwargs)
         return fig, ax
