e64e18e10daad4794863b24f380aed1df5dfaac3
SBouchard01 <simonbouchard47@gmail.com>
2026-07-31 02:52:16 +0200

    feat: refactor best_fit.py with the new compression format

--- scripts/bgs/measurements/best_fit.py
+++ scripts/bgs/measurements/best_fit.py
@@ -1,168 +1,112 @@
-"""
-Find the best-fit HOD based on chi-squared statistic.
-Note that this requires all the HOD folders to have all their measurements computed for the given cosmology (otherwise the compression functions will crash).
-
-Usage:
-    python best_fit.py --cosmology 0 --ells 0 2 --ndof 5 --diag --plot --log_level INFO
-"""
-import logging
-import argparse
-import numpy as np
+import argparse  # noqa: INP001
 from pathlib import Path
+
+import lsstypes
 import matplotlib.pyplot as plt
-from pycorr import TwoPointEstimator
-from acm.utils.modules import get_class_from_module
-from acm.utils.logging import setup_logging
- 
-def load_ref(dir: str|Path, phase: int = 0, los: list = ['x', 'y', 'z'], rebin: int = 1, Mr: float = -20, **kwargs):
+import numpy as np
+import yaml
+from compress_files import select
+from measure_box import get_estimator
+
+from acm.estimators.compression import Compressor
+from acm.utils.logging import get_logger_for_script, setup_logging
+from acm.utils.scripts import NumpyLoader
+
+logger = get_logger_for_script(__file__)
+
+def chi2(
+    observed: np.ndarray[tuple[int], np.dtype[np.float64]],
+    expected: np.ndarray[tuple[int], np.dtype[np.float64]],
+    covariance: np.ndarray[tuple[int, int], np.dtype[np.float64]],
+    dof: int = 1,
+) -> float:
     """
-    Load reference measurements from the specified directory.
-    
+    Calculate the chi-squared statistic.
+
     Parameters
     ----------
-    dir : str | Path
-        Directory containing the measurement files.
-    phase : int
-        Phase index to load. Defaults to 0.
-    los : list
-        List of line-of-sight directions to sum on. Defaults to ['x', 'y', 'z'].
-    rebin : int
-        Rebinning factor to apply to the measurements. Defaults to 1 (no rebinning).
-    Mr : float
-        Magnitude threshold for the measurements (defines a subdirectory in the path). Defaults to -20.
-    **kwargs
-        Additional keyword arguments to pass to the TwoPointEstimator call.
-        
+    observed : np.ndarray
+        The observed data points (1D array).
+    expected : np.ndarray
+        The expected data points (1D array).
+    covariance : np.ndarray
+        The covariance matrix of the data (2D array).
+    dof : int, optional
+        The number of degrees of freedom (number of data points minus number of fitted parameters).
+        If not provided, defaults to 1 (i.e., no adjustment for degrees of freedom).
+
     Returns
     -------
-    np.ndarray | tuple
-        The esimated correlation function multipoles, rebinned as specified.
-        If return_sep is True, returns (separations, multipoles), else returns multipoles only.
+    float
+        The chi-squared statistic.
     """
-    # TODO: Find a way to not encode path structure this strictly ? (how?)
-    dir = Path(dir) / f'AbacusSummit_base_c000_ph{phase:03d}' / 'measurements' / f'Mr{Mr}' 
-    fns = [dir / f'tpcf_los_{l}.npy' for l in los]
-    cf = sum([TwoPointEstimator.load(fn).normalize() for fn in fns])
-    cout = cf[::rebin](**kwargs)
-    return cout
-
-def load_covariance(dir: str|Path, n_phases: int = 25, Mr: float = -20, **kwargs):
-    """
-    Load the covariance matrix from the specified directory.
-    Calls load_ref for each phase and computes the covariance.
-    
-    Parameters
-    ----------
-    dir : str | Path
-        Directory containing the measurement files.
-    n_phases : int
-        Number of phases to load.
-    Mr : float
-        Magnitude threshold for the measurements (defines a subdirectory in the path). Defaults to -20.
-    **kwargs
-        Additional keyword arguments to pass to the load_ref call (return_sep will be enforced to False).
-    
-    Returns
-    -------
-    np.ndarray
-        Covariance matrix computed from the loaded measurements.
-    """
-    kwargs['return_sep'] = False # Ensure we only get the data arrays
-    cfs = [load_ref(dir, phase=phase, Mr=Mr, **kwargs) for phase in range(n_phases)]
-    cov = np.cov([cf.flatten() for cf in cfs], rowvar=False)
-    return cov
-
-def chi2(observed, expected, covariance):
-    """Calculate the chi-squared statistic."""
     diff = observed - expected
     inv_cov = np.linalg.inv(covariance)
     chi2 = diff @ inv_cov @ diff
-    return chi2
-
-def chi2_ndof(observed, expected, covariance, dof):
-    """Calculate the chi-squared per degree of freedom."""
-    chi2_value = chi2(observed, expected, covariance)
-    return chi2_value / dof
-
-def plot_best_fit(s, ref_poles, best_poles, ells, hod):
-    """Plot the best-fit comparison."""
-    fig, ax = plt.subplots()
-
-    for i, p in enumerate(ells):
-        ax.plot(s, ref_poles[i]*s**2, ls='--', c=f'C{i}')
-        ax.plot(s, best_poles[i]*s**2, c=f'C{i}')
-    
-    handles = [
-        plt.Line2D([0], [0], color='k', ls='--', label='SecondGen mock'),
-        plt.Line2D([0], [0], color='k', label=f'Best HOD fit (HOD {hod})'),
-    ]
-    ax.legend(handles=handles)
-    ax.set_title('Best-fit HOD comparison')
-    ax.set_xlabel('s [Mpc/h]')
-    ax.set_ylabel(r'$s^2 \xi_\ell(s)$ [Mpc/h]$^2$')
-    return fig, ax
-
+    return chi2 / dof
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Find best-fit HOD based on chi-squared statistic.')
-    parser.add_argument('--module', type=str, default='acm.observables.bgs', help='Base module path for observables')
-    parser.add_argument('--measurements_dir', '-md', type=str, default=None, required=True, help='Directory containing measurement files')
-    parser.add_argument('--param_dir', '-pd', type=str, default=None, required=True, help='Directory containing HOD parameter files')
-    parser.add_argument('--cosmology', '-c', type=int, default=0, help='Cosmology index to find the fit for (default: 0)')
+    parser = argparse.ArgumentParser(description="Calculate data best-fit statistics")
+    parser.add_argument("--root_obs", type=str, required=True, help="Root directory containing the measurements")
+    parser.add_argument("--root_exp", type=str, required=True, help="Root directory containing the expected measurements")
+    parser.add_argument("--measurement", type=str, required=True, help="Measurement to process")
+    parser.add_argument("--estimator_config", type=str, required=True, help="YAML file containing estimator parameters.")
     parser.add_argument('--Mr', type=float, default=-20, help='Magnitude threshold for the measurements (default: -20)')
-    parser.add_argument('--ells', '-e', type=int, nargs='+', default=[0], help='Multipole moments to consider (default: 0)')
-    parser.add_argument('--ndof', '-n', type=int, help='Calculate chi-squared per degree of freedom')
-    parser.add_argument('--diag', '-d', action='store_true', help='Use diagonal covariance matrix only')
-    parser.add_argument('--plot', '-p', action='store_true', help='Plot the best-fit comparison')
-    parser.add_argument('--log_level', type=str, default='warning', help='Set logging level (e.g., DEBUG, INFO)')
+    parser.add_argument('--cosmo_idx', type=int, default=0, help='Cosmology index to use (default: 0)')
+    parser.add_argument('--ndof', type=int, help='Calculate chi-squared per degree of freedom')
+    parser.add_argument('--diag', action='store_true', help='Use diagonal covariance matrix only')
+    parser.add_argument('--plot', action='store_true', help='Plot the best-fit comparison')
+    parser.add_argument("--log_level", type=str, default='warning', help="Set logging level (e.g., DEBUG, INFO)")
     args = parser.parse_args()
-    ells = args.ells
-    
-    logger = logging.getLogger(__file__.split('/')[-1])
+
     setup_logging(level=args.log_level)
-    
-    logger.info(f'Using multipoles: {ells}')
-    logger.info(f'Calculating chi-squared {f"with {args.ndof} degree of freedom" if args.ndof else ""}')
 
-    cf_kwargs = dict(ells=ells, rebin=1) # Enforce no rebinning as we fit the CFs
-    
-    paths = dict(
-        measurements_dir = args.measurements_dir,
-        param_dir = args.param_dir
-    )
-    
-    # Load compressed measurements for the specified cosmology
-    obs = get_class_from_module(args.module, 'tpcf')
-    data = obs.compress_data(paths=paths, cosmos=[args.cosmology], **cf_kwargs)
-    y = obs.flatten_output(data.y, flat_output_dims=2).values # to 2D numpy array
-    hods = obs.get_hod_from_files(paths=paths, cosmo_idx=args.cosmology)
-    logger.info(f'Loaded data for cosmology c{args.cosmology:03d} with shape {data.y.shape}')
-    
-    # Load reference measurement and covariance
-    ref_dir = '/pscratch/sd/s/sbouchar/SecondGen/CubicBox/BGS/z0.200'
-    ref_poles = load_ref(ref_dir, Mr=args.Mr, **cf_kwargs)
-    cov = load_covariance(ref_dir, Mr=args.Mr, **cf_kwargs)
-    if args.diag:
-        cov = np.diag(np.diag(cov)) # Use diagonal covariance only
-    
-    # TODO: Add covariance sanity checks here
-    
-    chi2_values = []
-    expected = ref_poles.flatten()
-    for cf in y:
-        observed = cf
-        
-        dof = len(observed) - args.ndof if args.ndof else 1 # Default dof=1 if not specified
-        chi2_value = chi2_ndof(observed, expected, cov, dof)
-        
-        chi2_values.append(chi2_value)
-        logger.debug(f'Chi-squared: {chi2_value}')
-    best_idx = np.argmin(chi2_values)
+    with Path(args.estimator_config).open() as f:
+        estimator_config = yaml.load(f, Loader=NumpyLoader)  # noqa: S506
 
-    print(f'c{args.cosmology:03d} Best-fit index: {best_idx} (HOD {hods[best_idx]}) with chi-squared: {chi2_values[best_idx]}')
+    stat_name = args.measurement
+    args = estimator_config.get(stat_name, {})
+    load_args = args.get("load", {})
+    reader = get_estimator(stat_name).load
+
+    # NOTE: using hardcoded pattern/index structure for those files, as they handle outputs of measure_box.py
+    pattern_obs = f"c{args.cosmo_idx}" + r"_ph{phase_idx}/seed{seed}/hod{hod_idx}/" + stat_name + r"_los-{los}.h5"
+    pattern_exp = f"AbacusSummit_base_c{args.cosmo_idx}" + r"_ph{phase_idx}/measurements/" + f"Mr{args.Mr}/{stat_name}" + r"_los-{los}.h5"
+    ignore_index = ["los"]
+
+    # Compress measurements
+    compressor = Compressor(root=Path(args.root_obs), pattern=pattern_obs)
+    group_obs = compressor.read(reader=reader, ignore_index=ignore_index, **load_args)
+    group_obs = select(stat_name, group_obs)
+    group_obs = group_obs.merge(method=lsstypes.mean)  # Merge identical indices
+    data = np.array([obj.data for obj in group_obs]) # Flattened arrays of observed data
+
+    # Load expected data and covariance matrix
+    compressor = Compressor(root=Path(args.root_exp), pattern=pattern_exp)
+    group_exp = compressor.read(reader=reader, ignore_index=ignore_index, **load_args)
+    group_exp = select(stat_name, group_exp)
+    group_exp = group_exp.merge(method=lsstypes.mean)  # Merge identical indices
+    expected = np.asarray(group_exp[0].data) # Flattened array of expected data
+    covariance = np.cov(np.array([obj.data for obj in group_exp]), rowvar=False)
+
+    values = []
+    for observed in data:
+        dof = len(observed) - args.ndof if args.ndof else 1
+        chi2_value = chi2(observed, expected, covariance, dof=dof)
+        logger.info(f"Chi-squared value: {chi2_value}")
+
+    idx = np.argmin(values)
+    str_idx = ", ".join([f"{k}={v}" for k, v in group_obs[idx].indexes.items()])
+    logger.info(f"Best-fit found for {str_idx} ({idx=}) with chi-squared value: {values[idx]}")
 
     if args.plot:
-        s = data['s'].values
-        best_poles = y[best_idx].reshape(len(ells), -1)
-        fig, ax = plot_best_fit(s, ref_poles, best_poles, ells, hods[best_idx])
-        fig.savefig(f'best_fit_c{args.cosmology:03d}_ells{tuple(ells)}.png', dpi=300, bbox_inches='tight')
+        plotter = get_estimator(stat_name).plot
+        fig, ax = plotter(group_obs[idx], ls='-')
+        fig, ax = plotter(group_exp[0], ls='--', fig=fig, ax=ax)
+        handles = [
+            plt.Line2D([0], [0], lw=2, ls='-', label='Observed'),
+            plt.Line2D([0], [0], lw=2, ls='--', label='Expected')
+        ]
+        ax.legend(handles=handles)
+        ax.set_title(f'Best-fit comparison for {stat_name} ({str_idx})')
+        fig.savefig(f"best_fit_{stat_name}.png", dpi=300, bbox_inches='tight')
