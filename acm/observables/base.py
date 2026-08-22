e4e4c0199c58ff39afa3bbd304970dc483ee047d
SBouchard01 <simonbouchard47@gmail.com>
2026-08-22 11:36:25 +0200

    feat: update base observable framework to better define I/O and objects
    - now returning 2d arrays, with optional unflatteded array and optional raw object
    - Deprecate 1D flattening
    - Apply selection on a 2D array

--- acm/observables/base.py
+++ acm/observables/base.py
@@ -10,71 +10,49 @@ from .model import ObservableModel
 
 logger = logging.getLogger(__name__)
 
-class Formatter[R, T](ABC): # NOTE: splitting interface for clarity
-    """Class handling the output formatting of the Observable product interface."""
+class Formatter[R, N](ABC): # NOTE: splitting interface for clarity
+    """
+    Class handling the output formatting of the Observable product interface.
 
-    def __init__(self, **kwargs) -> None:
-        self.set_output(**kwargs)
+    Dynamic type parameters:
+    - R: Raw data type, with or without selections applied.
+    - N: 2D NumPy array type for the output.
+    """
+
+    def __init__(self) -> None:
         self._filters: dict = {}
         self._select: list[int] | None = None
         self._select_names: list[str] = []
 
-    def set_output(
-        self,
-        numpy: bool = False,
-        squeeze: bool = True,
-        flatten: int | None = None,
-    ) -> None:
-        """
-        Set the output format of the current instance.
-
-        Parameters
-        ----------
-        numpy : bool, optional
-            If True, the output will be a NumPy array.
-            If False, the output will match the data type. Defaults to False.
-        squeeze : bool, optional
-            If True, the output will be squeezed to remove single-dimensional entries.
-            Defaults to True.
-        flatten : int or None, optional
-            Flatten to the specified number of dimensions.
-            Currently supports 1D and 2D flattening.
-            Otherwise, no flattening is applied. Defaults to None.
-
-        Raises
-        ------
-        ValueError
-            If flatten is not None and not in [1, 2].
-        """
-        if flatten is not None and flatten not in [1, 2]:
-            raise ValueError("Flattening is only supported for 1D and 2D outputs.")
-        logger.info(f"Setting output format: {numpy=}, {squeeze=}, {flatten=}")
-        self._output_numpy = numpy
-        self._output_squeeze = squeeze
-        self._output_flatten = flatten
-
+    @property
+    def filters(self) -> dict:
+        """Get the current filters."""
+        return self._filters
+    @filters.setter
+    def filters(self, filters: dict) -> None:
+        """Set the filters and update related values."""
+        logger.debug(f"Filters set: {filters}")
+        self._filters = filters
     def set_filters(self, **kwargs) -> None:
+        """Explicitly set filters trough keyword arguments."""
+        self.filters = kwargs # Uses setter
+
+    def set_selection(self, *names: str, indices: list[int]) -> None:
         """
-        Set filters for the current instance.
+        Set the indice selection on the last dimension of the 2D NumPy array output.
+
+        Only applied on elements matching the registered names.
 
         Parameters
         ----------
-        **kwargs
-            Keyword arguments specifying the filter criteria.
-
-        Examples
-        --------
-        >>> obs.set_filter(cosmo=0, ells=[0, 2], s=slice(0, 10))
-        # Will select the coordinates 'cosmo' 0, 'ells' 0 or 2 and 's' in [0, 10].
+        names: str
+            Names of the elements to which the selection will be applied.
+        indices: list[int]
+            Indices to select from the last dimension of the 2D NumPy array output.
         """
-        self._filters = kwargs
-        logger.info(f"Filter set: {kwargs}")
-
-    def set_select(self, *names: str, indices: list[int]) -> None:
-        """Set selection indices for 1D or 2D-formatted outputs."""
-        self._select_names = list(names)
         self._select = indices
-        logger.info(f"Selection indices set: {indices}")
+        self._select_names = names
+        logger.debug(f"Selection set: {indices=}, {names=}")
 
     def clear_filters(self) -> None:
         """Clear all filters and selection indices."""
@@ -88,89 +66,145 @@ class Formatter[R, T](ABC): # NOTE: splitting interface for clarity
         """Apply the set filters to the provided data."""
 
     @abstractmethod
-    def _apply_selection(self, data: R) -> R:
-        """Apply the set selection indices to the provided data."""
+    def _apply_selection(self, data: N, name: str) -> N:
+        """Select specified indices from a 2D NumPy array."""
 
-    @abstractmethod
     @staticmethod
-    def _flatten(data: R, ndim: int) -> R:
-        """Flatten the provided data to the specified number of dimensions."""
-
     @abstractmethod
-    @staticmethod
-    def _squeeze(data: R) -> R:
-        """Squeeze the provided data."""
-
-    @abstractmethod
-    @staticmethod
-    def _to_numpy(data: R) -> np.ndarray:
-        """Cast the provided data to a NumPy array."""
-
-    def _format_data(self, data: R) -> R | np.ndarray:
+    def _to_numpy(data: R, nested: bool = False) -> N:
         """
-        Format the provided data according to the set filters, selection, and output.
+        Cast the provided data to a NumPy array.
+
+        Parameters
+        ----------
+        data: R
+            The object to cast on a numpy array.
+        nested: bool
+            If True, returns an unflattened array. Defaults to False (2D array)
+
+        Returns
+        -------
+        N
+            The data cast to a 2D NumPy array, unless nested=True.
+        """
+
+    def _format_data(self, data: R, name: str, nested: bool = False) -> N:
+        """
+        Format the provided data by applying filters, casting to numpy and applying selection.
 
         Parameters
         ----------
         data: R
             The data to format.
+        name: str
+            The name of the data variable, used for selection.
+        nested: bool, optional
+            If True, the data is nested and will not be flattened. Default is False.
 
         Returns
         -------
-        R or np.ndarray
-            The formatted data, either as the original type or as a NumPy array.
+        np.ndarray
+            The formatted data as a 2D NumPy array, unless nested=True.
         """
-        out = self._apply_filters(data)
-        if self._output_flatten is not None:
-            out = self._flatten(out, ndim=self._output_flatten)
-        out = self._apply_selection(out)
-        if self._output_squeeze:
-            out = self._squeeze(out)
-        if self._output_numpy:
-            out = self._to_numpy(out)
-        return out
+        data = self._apply_filters(data)
+        data = self._to_numpy(data, nested=nested)
+        if not nested:
+            data = self._apply_selection(data, name)
+        return data
 
     @overload
-    def get_data(self, name: str, raw: Literal[True]) -> R: ...
+    def get_data(self, name: str, raw: Literal[True], nested: bool = False) -> R:
+        ...
     @overload
-    def get_data(self, name: str, raw: Literal[False] = False) -> T: ...
+    def get_data(self,
+        name: str,
+        raw: Literal[False] = False,
+        nested: bool = False,
+    ) -> N:
+        ...
     @abstractmethod
-    def get_data(self, name: str, raw: bool = False) -> T:
-        """Get the matching data object of the observable instance."""
+    def get_data(self, name, raw = False, nested = False):
+        """
+        Get the data for the given name.
+
+        Parameters
+        ----------
+        name: str
+            The name of the data to retrieve.
+        raw: bool, optional
+            If True, return the raw unfiltered and unflattened data. Default is False.
+        nested: bool, optional
+            If True, return the data in its original unflattened form. Default is False.
+        """
 
     @overload
-    def get_prediction(self, x: T, raw: Literal[True]) -> R: ...
+    def get_prediction(self, x: N, raw: Literal[True], nested: bool = False) -> R:
+        ...
     @overload
-    def get_prediction(self, x: T, raw: Literal[False] = False) -> T: ...
+    def get_prediction(self,
+        x: N,
+        raw: Literal[False] = False,
+        nested: bool = False,
+    ) -> N:
+        ...
     @abstractmethod
-    def get_prediction(self, x: T, raw: bool = False) -> T:
-        """Wrap around :meth:`ObservableModel.get_prediction` with formatting."""
+    def get_prediction(self, x: N, raw = False, nested = False):
+        """
+        Get the prediction for the given input x.
+
+        Parameters
+        ----------
+        x: N
+            The input data for which to get the prediction.
+        raw: bool, optional
+            If True, return the raw unfiltered and unflattened prediction. Default is False.
+        nested: bool, optional
+            If True, return the prediction in its original unflattened form. Default is False.
+        """
+        # TODO: Final transform (e.g. phase correction) - Here or in model?
 
     @overload
-    def get_model_error(self, method: str, raw: Literal[True], **kwargs) -> R: ...
+    def get_model_error(
+        self,
+        method: str,
+        raw: Literal[True],
+        nested: bool = False,
+        **kwargs,
+    ) -> R:
+        ...
     @overload
-    def get_model_error(self, method: str, raw: Literal[False] = False, **kwargs) -> T: ...
+    def get_model_error(
+        self,
+        method: str,
+        raw: Literal[False] = False,
+        nested: Literal[False] = False,
+        **kwargs,
+    ) -> N:
+        ...
     @abstractmethod
-    def get_model_error(self, method: str, raw: bool = False, **kwargs) -> T:
-        """Wrap around :meth:`ObservableModel.get_error` with formatting."""
+    def get_model_error(self, method, raw = False, nested = False, **kwargs):
+        """
+        Wrap around :meth:`ObservableModel.get_error`.
 
-    @abstractmethod
-    def get_model_covariance(self, prefactor: float = 1, **kwargs) -> np.ndarray:
-        """Wrap around :meth:`ObservableModel.make_covariance` with formatting."""
+        Parameters
+        ----------
+        method: str
+            The method to use for the model error calculation.
+        raw: bool, optional
+            If True, return the raw unfiltered and unflattened error. Default is False.
+        nested: bool, optional
+            If True, return the error in its original unflattened form. Default is False.
+        **kwargs
+            Additional keyword arguments for the model error calculation.
+        """
 
 
 #%% Product components
-class BaseObservable[R, T](Formatter[R, T], ABC):
-    """
-    Base class defining the interface for all Observable classes.
+class BaseObservable[R, N](Formatter[R, N], ABC):
+    """Base class defining the interface for all Observable classes."""
 
-    Dynamically typed with two type variables:
-    - R: The raw data type (e.g., :class:`xarray.DataArray`, etc.)
-    - T: The default data type, usually an union between `R` and :class:`numpy.ndarray`
-    """
-
-    def __init__(self, model: ObservableModel | None = None, **kwargs) -> None:
-        super().__init__(**kwargs)
+    def __init__(self, model: ObservableModel | None = None) -> None:
+        super().__init__()
         self.model = model # Public attribute
 
     def __copy__(self) -> Self:
@@ -189,18 +223,18 @@ class BaseObservable[R, T](Formatter[R, T], ABC):
     def _copy(self, deep: bool = False, **kwargs) -> Self:
         """Create a copy of the current instance."""
 
-    @abstractmethod
     @classmethod
+    @abstractmethod
     def load(cls, filename: str | Path, **kwargs) -> Self:
         """Load an observable instance from a file."""
 
-    @abstractmethod
     @classmethod
+    @abstractmethod
     def can_load(cls, filename: str | Path) -> bool:
         """Determine if the class can load the given file."""
 
-    @abstractmethod
     @property
+    @abstractmethod
     def x_names(self) -> list[str]:
         """List of the parameter names."""
 
@@ -232,8 +266,11 @@ class BaseObservable[R, T](Formatter[R, T], ABC):
         """
         cov_y = self.get_data("covariance_y", raw=True)
         cov_y = self._apply_filters(cov_y)
-        cov_y = self._apply_selection(cov_y)
-        cov_y = self._flatten(cov_y, ndim=2)
         cov_y = self._to_numpy(cov_y)
+        cov_y = self._apply_selection(cov_y, "y")
         cov = prefactor / volume_factor * np.cov(cov_y, rowvar=False)
         return cov
+
+    @abstractmethod
+    def get_model_covariance(self, prefactor: float = 1, **kwargs) -> np.ndarray:
+        """Wrap around :meth:`ObservableModel.make_covariance` with formatting."""
