bc9acefc891dce1d15a99f1b5bdc0350e22bbb74
SBouchard01 <simonbouchard47@gmail.com>
2026-08-31 18:50:37 +0200

    fix: remove conftest with jax mock to avoid scipy detecting MagicMock type and crashing

--- tests/acm/utils/conftest.py
+++
@@ -1,8 +0,0 @@
-import sys
-from unittest.mock import MagicMock
-
-# ruff: noqa: ANN001, ANN201, D103
-
-def pytest_configure(config):  # noqa: ARG001
-    sys.modules.setdefault("jax", MagicMock())
-
