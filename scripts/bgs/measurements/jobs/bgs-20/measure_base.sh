69e9222c3f48a3d3eaed30a6795fa44ce3e0e12c
SBouchard01 <simonbouchard47@gmail.com>
2026-09-09 20:50:59 +0200

    fix: update file paths in configuration and script files for consistency across measurements

--- scripts/bgs/measurements/jobs/bgs-20/measure_base.sh
+++ scripts/bgs/measurements/jobs/bgs-20/measure_base.sh
@@ -6,7 +6,7 @@
 #SBATCH --qos regular
 #SBATCH --constraint gpu&hbm80g
 
-#SBATCH --time 48:00:00
+#SBATCH --time 12:00:00
 
 #SBATCH --job-name bgs_base
 #SBATCH --output /pscratch/sd/s/sbouchar/Output_jobs/bgs-20_measurements/%A.%x_%a.out
@@ -18,13 +18,15 @@ source /global/common/software/desi/users/adematti/cosmodesi_environment.sh main
 # Load the old pyrecon module for densitysplit
 module swap pyrecon/mpi pyrecon/main
 
+export XLA_PYTHON_CLIENT_ALLOCATOR=platform # JAX backend for GPU memory allocation
+
 # Get the cosmology index from the SLURM_ARRAY_TASK_ID
 COSMO_LIST=(0 {1..4} 13 {100..126} {130..181}) # List of cosmologies to be used
 ID=$((SLURM_ARRAY_TASK_ID)) # ID of the cosmology to be used, starting from 0
 COSMO=${COSMO_LIST[ID]} # Cosmology to be used
 
 RUN=1
-LOGFILE=$(printf "/pscratch/sd/s/sbouchar/acm/bgs-v2.0/mr-20/logs/run%d/measurements/base/log_c%03d_ph000_seed0.log" ${RUN} ${COSMO})
+LOGFILE=$(printf "/pscratch/sd/s/sbouchar/acm/bgs/mr-20/logs/v2.0/measurements/abacus/base/run%d/log_c%03d_ph000_seed0.log" ${RUN} ${COSMO})
 
 cd /global/homes/s/sbouchar/acm/scripts/bgs/measurements
 python measure_box.py --config jobs/bgs-20/config.yaml --cosmologies ${COSMO} --log_file "${LOGFILE}"
